ff677801a81f6bff6e834c6700542ce1f46852cd
yt-dlx <yt-dlx@proton.me>
2024-05-15 17:34:43 +0530

    version 2

--- project/types/base/Agent.d.ts
+++
@@ -1,7 +0,0 @@
-import type { EngineOutput } from "./Engine";
-export default function Agent({ query, verbose, onionTor, }: {
-    query: string;
-    verbose?: boolean;
-    onionTor?: boolean;
-}): Promise<EngineOutput>;
-//# sourceMappingURL=Agent.d.ts.map
--- project/types/base/retor.d.ts
+++
@@ -1,6 +0,0 @@
-declare const retor: () => Promise<{
-    sysip: any;
-    torip: any;
-}>;
-export default retor;
-//# sourceMappingURL=retor.d.ts.map
