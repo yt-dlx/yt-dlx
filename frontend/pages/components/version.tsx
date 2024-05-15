c0c3e7a96c34a828829a273dfa648c2e237bc65d
yt-dlx <yt-dlx@proton.me>
2024-05-15 17:54:05 +0530

    version 3

--- frontend/pages/components/version.tsx
+++
@@ -1,9 +0,0 @@
-export default function VerPackage() {
-  return (
-    <div className="space-y-6">
-      <h1 className="text-5xl text-red-600 font-bold lg:text-9xl">
-        YT-DLX@8.4.0
-      </h1>
-    </div>
-  );
-}
