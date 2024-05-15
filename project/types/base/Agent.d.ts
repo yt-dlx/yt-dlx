d8b0393d10e84c558ca2c7b2910b2d12b9862e9d
yt-dlx <yt-dlx@proton.me>
2024-05-15 19:43:54 +0530

    client:4 /> npm:8.6.0

--- project/types/base/Agent.d.ts
+++
@@ -1,6 +0,0 @@
-import type { EngineOutput } from "./Engine";
-export default function Agent({ query, verbose, }: {
-    query: string;
-    verbose?: boolean;
-}): Promise<EngineOutput>;
-//# sourceMappingURL=Agent.d.ts.map
