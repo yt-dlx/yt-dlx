abb4a24db48849e06d27c1872e4cf82ef38752a4
yt-dlx <yt-dlx@proton.me>
2024-05-15 18:23:09 +0530

    client:3 /> npm:8.5.0

--- core/base/niptor.ts
+++ core/base/niptor.ts
@@ -3,12 +3,17 @@ import { spawn } from "child_process";
 
 export async function checkSudo() {
   return new Promise<boolean>((resolve) => {
-    if (process.env.sudo === "false") resolve(false);
-    const check = spawn("sudo", ["-n", "true"]);
-    check.on("error", () => resolve(false));
-    check.on("close", (code) => {
-      resolve(code === 0);
-    });
+    try {
+      if (process.env.sudo === "false") resolve(false);
+      const check = spawn("sudo", ["-n", "true"]);
+      check.on("error", () => resolve(false));
+      check.on("close", (code) => {
+        resolve(code === 0);
+      });
+    } catch (error) {
+      console.error(error);
+      resolve(false);
+    }
   });
 }
 
