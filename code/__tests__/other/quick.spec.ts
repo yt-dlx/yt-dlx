01a9d1c02fec901a690626e631262457d75a1111
yt-dlx <yt-dlx@proton.me>
2024-05-17 12:05:28 +0530

    client:5 /> npm:8.7.0

--- code/__tests__/other/quick.spec.ts
+++ code/__tests__/other/quick.spec.ts
@@ -1,1 +1,40 @@
 console.clear();
+import { execSync } from "child_process";
+
+function CheckCommand(command: string) {
+  try {
+    execSync(`${command} --version`);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
+async function main() {
+  const commandsToCheck = ["sudo", "systemctl", "service", "tor"];
+  for (const command of commandsToCheck) {
+    let isAvailable: boolean;
+    switch (command) {
+      case "sudo":
+        isAvailable = CheckCommand("sudo");
+        break;
+      case "systemctl":
+        isAvailable = CheckCommand("systemctl");
+        break;
+      case "pkill":
+        isAvailable = CheckCommand("pkill");
+        break;
+      case "tor":
+        isAvailable = CheckCommand("tor");
+        break;
+      default:
+        console.log(`Unknown command: ${command}`);
+        isAvailable = false;
+        break;
+    }
+    if (isAvailable) console.log(`'${command}' command is available.`);
+    else console.log(`'${command}' command is not available.`);
+  }
+}
+
+main();
