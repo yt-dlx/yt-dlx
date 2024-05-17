b2793760415432d2c72977eb2cacbcee22f10103
yt-dlx <yt-dlx@proton.me>
2024-05-17 14:19:36 +0530

    client:5 /> npm:8.7.0

--- code/__tests__/other/quick.spec.ts
+++ code/__tests__/other/quick.spec.ts
@@ -1,1 +1,19 @@
 console.clear();
+import cip from "../../base/check/cip";
+import { execSync } from "child_process";
+import cservice from "../../base/check/cservice";
+import csystemctl from "../../base/check/csystemctl";
+
+switch (true) {
+  case cservice():
+    execSync("service tor restart", { stdio: "inherit" });
+    console.log("@ipAddress", cip(true));
+    break;
+  case csystemctl():
+    execSync("systemctl restart tor", { stdio: "inherit" });
+    console.log("@ipAddress", cip(true));
+    break;
+  default:
+    console.log("@ipAddress", cip(false));
+    break;
+}
