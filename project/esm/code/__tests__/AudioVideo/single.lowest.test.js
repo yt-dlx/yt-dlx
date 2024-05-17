6f1209dd67475aa83915bece85b738733674c5b7
yt-dlx <yt-dlx@proton.me>
2024-05-17 10:39:05 +0530

    client:4 /> npm:8.6.0

---
+++ project/esm/code/__tests__/AudioVideo/single.lowest.test.js
@@ -0,0 +1,20 @@
+// =============================[ CORE TESTER ]=============================
+//
+import colors from "colors";
+import YouTube from "../../";
+(async () => {
+    try {
+        await YouTube.AudioVideo.Single.Lowest({
+            stream: false,
+            verbose: true,
+            output: "public/mix",
+            query: "21 savage - redrum",
+        });
+    }
+    catch (error) {
+        console.error(colors.red(error.message));
+    }
+})();
+//
+// =============================[ CORE TESTER ]=============================
+//# sourceMappingURL=single.lowest.test.js.map
---
+++ project/types/base/Agent.d.ts
@@ -0,0 +1,6 @@
+import type { EngineOutput } from "./Engine";
+export default function Agent({ query, verbose, }: {
+    query: string;
+    verbose?: boolean;
+}): Promise<EngineOutput>;
+//# sourceMappingURL=Agent.d.ts.map
