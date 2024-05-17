0f2b8d94fd8571bde5f3edc61e9845b0fc37459a
yt-dlx <yt-dlx@proton.me>
2024-05-17 11:26:42 +0530

    client:5 /> npm:8.7.0

--- frontend/pages/components/version.tsx
+++ frontend/pages/components/version.tsx
@@ -2,7 +2,7 @@ export default function VerPackage() {
   return (
     <div className="space-y-6">
       <h1 className="text-5xl text-red-600 font-bold lg:text-9xl">
-        YT-DLX@8.6.0
+        YT-DLX@8.7.0
       </h1>
     </div>
   );
