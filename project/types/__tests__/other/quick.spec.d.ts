3680e1b7c95cdb43f741240b1b2faa175176712b
yt-dlx <yt-dlx@proton.me>
2024-05-22 12:18:55 +0530

    client:8 /> npm:9.0.0

--- project/types/__tests__/other/quick.spec.d.ts
+++
@@ -1,6 +0,0 @@
-export default function Agent({ query, useTor, verbose, }: {
-    query: string;
-    useTor?: boolean;
-    verbose?: boolean;
-}): Promise<any>;
-//# sourceMappingURL=quick.spec.d.ts.map
