9b70505572cec4eab02dbee8f598b41df1ea5d04
yt-dlx <yt-dlx@proton.me>
2024-05-24 11:14:11 +0530

    client:9 /> npm:9.0.0

--- package/scripts/main.py
+++
@@ -1,9 +0,0 @@
-import sys, time
-
-
-def square_of_number():
-    print(int(sys.argv[1]) ** 2)
-
-
-if __name__ == "__main__":
-    square_of_number()
--- project/types/__tests__/Command/help.test.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=help.test.d.ts.map
