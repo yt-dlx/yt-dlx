311bb1c6efe4f95fee3bf21bcdb2d08abdfd02d9
yt-dlx <yt-dlx@proton.me>
2024-05-26 08:32:21 +0530

    client:9 /> npm:10.0.0

---
+++ project/cjs/code/__tests__/Command/search_playlists.test.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const __1 = __importDefault(require("../../"));
+const colors_1 = __importDefault(require("colors"));
+(async () => {
+    try {
+        console.log(colors_1.default.blue("@test:"), "ytSearch playlist multiple");
+        const result = await __1.default.ytSearch.Playlist.Multiple({
+            query: "8k dolby nature",
+        });
+        console.log(result);
+    }
+    catch (error) {
+        console.error(colors_1.default.red(error.message));
+    }
+})();
+//# sourceMappingURL=search_playlists.test.js.map
