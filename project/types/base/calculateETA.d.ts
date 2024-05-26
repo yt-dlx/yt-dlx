38e0efa3b328abc0fa1491a56fddfee9a7709c24
yt-dlx <yt-dlx@proton.me>
2024-05-26 08:25:08 +0530

    client:9 /> npm:9.0.0

---
+++ project/types/base/calculateETA.d.ts
@@ -0,0 +1,9 @@
+/**
+ * Calculates the estimated time remaining based on the start time and completion percentage.
+ *
+ * @param startTime - The start time of the operation.
+ * @param percent - The completion percentage of the operation.
+ * @returns The estimated time remaining in seconds as a string with two decimal places.
+ */
+export default function calculateETA(startTime: Date, percent: number): string;
+//# sourceMappingURL=calculateETA.d.ts.map
