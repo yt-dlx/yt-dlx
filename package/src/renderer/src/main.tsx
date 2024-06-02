6cb6d3f0873406b698ac50e561985b0de5e4f797
yt-dlx <yt-dlx@proton.me>
2024-06-02 13:28:35 +0530

    client:10 /> npm:10.0.0

---
+++ package/src/renderer/src/main.tsx
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom/client";
+import "./assets/main.css";
+import React from "react";
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
