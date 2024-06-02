287170f9101329e9469861fa049b68ec77848433
yt-dlx <yt-dlx@proton.me>
2024-06-02 14:03:54 +0530

    client:10 /> npm:10.0.0

--- code/interfaces/sizeFormat.ts
+++
@@ -1,3 +0,0 @@
-export default interface sizeFormat {
-  (filesize: number): string | number;
-}
--- package/src/renderer/src/pages/ContactPage.tsx
+++
@@ -1,5 +0,0 @@
-function ContactPage() {
-  return <h1>Contact Page</h1>;
-}
-
-export default ContactPage;
