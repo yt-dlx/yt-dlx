c2dc945f757378a3ed191e77201110a4394179c8
yt-dlx <yt-dlx@proton.me>
2024-06-07 08:39:04 +0530

    < core 10 />

--- out/esm/code/interfaces/VideoInfo.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=VideoInfo.js.map
--- out/types/base/ffbins.d.ts
+++
@@ -1,5 +0,0 @@
-declare const ffmpegPath: string;
-declare const ffplayPath: string;
-declare const ffprobePath: string;
-export { ffmpegPath, ffplayPath, ffprobePath };
-//# sourceMappingURL=ffbins.d.ts.map
