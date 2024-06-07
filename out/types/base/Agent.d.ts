4dbc903f59a1afb45f4f996e6666dfc36ceb0b6b
yt-dlx <yt-dlx@proton.me>
2024-06-07 09:08:08 +0530

    < core 10 />

---
+++ out/types/base/Agent.d.ts
@@ -0,0 +1,6 @@
+export default function Agent({ query, useTor, verbose, }: {
+    query: string;
+    useTor?: boolean;
+    verbose?: boolean;
+}): Promise<any>;
+//# sourceMappingURL=Agent.d.ts.map
---
+++ out/types/base/ffbins.d.ts
@@ -0,0 +1,5 @@
+declare const ffmpegPath: string;
+declare const ffplayPath: string;
+declare const ffprobePath: string;
+export { ffmpegPath, ffplayPath, ffprobePath };
+//# sourceMappingURL=ffbins.d.ts.map
