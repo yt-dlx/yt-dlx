2d1c2a34e5d15daecc8c5b0f93749b978f7b5815
yt-dlx <yt-dlx@proton.me>
2024-06-10 00:09:16 +0530

    < npm 12.4.0 />

---
+++ client/src/main/index.ts
@@ -0,0 +1,50 @@
+import { electronApp, optimizer, is } from "@electron-toolkit/utils";
+import { app, shell, BrowserWindow } from "electron";
+import icon from "../../resources/icon.png?asset";
+import ipcApi from "./ipcApi";
+import { join } from "path";
+
+function createWindow(): void {
+  var mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    show: false,
+    autoHideMenuBar: true,
+    ...(process.platform === "linux" ? { icon } : {}),
+    webPreferences: {
+      preload: join(__dirname, "../preload/index.js"),
+      sandbox: false,
+      webSecurity: false,
+      contextIsolation: false,
+    },
+  });
+  mainWindow.on("ready-to-show", () => {
+    mainWindow.show();
+  });
+  mainWindow.webContents.setWindowOpenHandler((details) => {
+    shell.openExternal(details.url);
+    return { action: "deny" };
+  });
+  if (is.dev && process.env["ELECTRON_RENDERER_URL"]) {
+    mainWindow.loadURL(process.env["ELECTRON_RENDERER_URL"]);
+  } else mainWindow.loadFile(join(__dirname, "../renderer/index.html"));
+}
+
+app.whenReady().then(() => {
+  electronApp.setAppUserModelId("com.electron");
+  app.on(
+    "browser-window-created",
+    (_: Electron.Event, window: BrowserWindow): void => {
+      optimizer.watchWindowShortcuts(window);
+    }
+  );
+  ipcApi();
+  createWindow();
+  app.on("activate", (): void => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", (): void => {
+  if (process.platform !== "darwin") app.quit();
+});
