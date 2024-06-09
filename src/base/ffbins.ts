70129388a47256c212764eb80c0c6b5392ba5634
yt-dlx <yt-dlx@proton.me>
2024-06-10 00:09:08 +0530

    < npm 12.4.0 />

--- src/base/ffbins.ts
+++
@@ -1,7 +0,0 @@
-import * as path from "path";
-
-const ffmpegPath: string = path.join(process.cwd(), "util", "ffmpeg");
-const ffplayPath: string = path.join(process.cwd(), "util", "ffplay");
-const ffprobePath: string = path.join(process.cwd(), "util", "ffprobe");
-
-export { ffmpegPath, ffplayPath, ffprobePath };
