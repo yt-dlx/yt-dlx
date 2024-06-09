d59e06b2617c324da65fd7bf1fcfa954621e8ff8
yt-dlx <yt-dlx@proton.me>
2024-06-10 00:13:25 +0530

    < npm 12.4.0 />

--- client/src/renderer/src/pages/docs/AudioVideo/AudioVideoHighest.tsx
+++ client/src/renderer/src/pages/docs/AudioVideo/AudioVideoHighest.tsx
@@ -14,7 +14,7 @@ export default function AudioVideoHighest(): JSX.Element {
             <article className="space-y-8">
               <div className="space-y-6">
                 <h1 className="text-5xl text-red-600 font-bold lg:text-9xl">
-                  YT-DLX@9.0.0
+                  YT-DLX@12.4.0
                 </h1>
               </div>
               <p className="text-white/60">
