539c6fb2c7e21d7d5b70279d6a41fc952561e4a9
yt-dlx <yt-dlx@proton.me>
2024-06-09 11:32:31 +0530

    < npm 13 />

--- util/ffdl.js
+++ util/ffdl.js
@@ -1,21 +1,26 @@
-import { createWriteStream, existsSync } from "fs";
-import { dirname, join } from "path";
-import { fileURLToPath } from "url";
-import fetch from "node-fetch";
+const axios = require("axios");
+const { join } = require("path");
+const { createWriteStream, existsSync } = require("fs");
 
 const binDL = async (url, filepath, binaryName) => {
   try {
-    const response = await fetch(url);
-    if (!response.ok) throw new Error(`@error: ${response.statusText}`);
-    const tSize = parseInt(response.headers.get("content-length"), 10);
+    const response = await axios({
+      url,
+      method: "GET",
+      responseType: "stream",
+    });
+    if (response.status !== 200) {
+      throw new Error(`@error: ${response.statusText}`);
+    }
+    const tSize = parseInt(response.headers["content-length"], 10);
     const writer = createWriteStream(filepath);
     let dSize = 0;
-    response.body.on("data", (chunk) => {
+    response.data.on("data", (chunk) => {
       dSize += chunk.length;
       const progress = Math.round((dSize / tSize) * 100);
       process.stdout.write(`@${binaryName}: ${progress}%\r`);
     });
-    response.body.pipe(writer);
+    response.data.pipe(writer);
     await new Promise((resolve, reject) => {
       writer.on("finish", resolve);
       writer.on("error", reject);
@@ -27,8 +32,6 @@ const binDL = async (url, filepath, binaryName) => {
 };
 
 (async () => {
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = dirname(__filename);
   const binaries = [
     {
       name: "ffmpeg.exe",
@@ -43,6 +46,7 @@ const binDL = async (url, filepath, binaryName) => {
       url: "https://github.com/yt-dlx/yt-dlx/releases/download/v1.0.0/ffprobe.exe",
     },
   ];
+
   for (const binary of binaries) {
     const filepath = join(__dirname, binary.name);
     if (!existsSync(filepath)) await binDL(binary.url, filepath, binary.name);
