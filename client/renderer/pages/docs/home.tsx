ff94738ab6811920f87e5f1811a514009b340ea3
yt-dlx <yt-dlx@proton.me>
2024-06-10 20:57:41 +0530

    < npm 13.0.0 />

--- client/renderer/pages/docs/home.tsx
+++ client/renderer/pages/docs/home.tsx
@@ -1,7 +1,9 @@
-import react from "react";
+import React from "react";
 import { motion } from "framer-motion";
+import NavPackage from "../components/nav";
+import FootPackage from "../components/foot";
+import Introduction from "../home/Introduction";
 import { FaClipboardCheck } from "react-icons/fa";
-import { HiFolderDownload } from "react-icons/hi";
 
 const FromLeftToRight = {
   initial: { opacity: 0, x: -100 },
@@ -23,9 +25,11 @@ const AndBounce = {
   exit: { opacity: 0, y: -50, transition: { duration: 0.3 } },
 };
 
-export default function Features(): JSX.Element {
+export default function HomePage(): JSX.Element {
   return (
-    <react.Fragment>
+    <main className="flex flex-col overflow-x-hidden max-h-screen scrollbar-thin bg-neutral-950 scrollbar-track-neutral-950 scrollbar-thumb-[#CD322D] font-semibold">
+      <NavPackage />
+      <Introduction />
       <motion.section
         id="Features"
         className="flex items-center justify-center border-b-8 border-double border-[#cd322d6e] w-full py-12 md:py-24 lg:py-32 bg-black text-white"
@@ -33,63 +37,49 @@ export default function Features(): JSX.Element {
         <div className="container px-4 md:px-6">
           <div className="grid items-center gap-6 lg:grid-cols-[1fr_500px] lg:gap-12 xl:grid-cols-[1fr_550px]">
             <div className="flex flex-col justify-center space-y-4">
-              <div className="space-y-2">
-                <motion.div
-                  className="inline-block rounded-2xl bg-[#CD322D] text-neutral-900 cursor-not-allowed font-black px-3 py-1 text-sm"
-                  {...FromLeftToRight}
-                >
-                  Key Features
-                </motion.div>
-                <motion.h2
-                  className="text-3xl font-black tracking-tighter sm:text-7xl text-[#CD322D]"
-                  {...FromLeftToRight}
-                >
-                  Powerful Multimedia Downloading
-                </motion.h2>
-                <motion.p
-                  className="max-w-[600px] text-white md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed"
-                  {...FromLeftToRight}
-                >
-                  Yt-Dlx allows you to download audio and video content from a
-                  wide range of sources, including YouTube, Vimeo, and many
-                  more. With its advanced capabilities, you can customize the
-                  download format, resolution, and more.
-                </motion.p>
-              </div>
+              <motion.div className="inline-block rounded-2xl bg-[#CD322D] text-neutral-900 cursor-not-allowed font-black px-3 py-1 text-sm">
+                placeholder
+              </motion.div>
+              <motion.h2
+                className="text-3xl font-black tracking-tighter sm:text-7xl text-[#CD322D]"
+                {...FromLeftToRight}
+              >
+                Laboris exercitation velit ex duis dolor labore deserunt.
+              </motion.h2>
               <motion.ul className="grid gap-2 py-4" {...AndBounce}>
                 <li>
                   <FaClipboardCheck className="mr-2 inline-block h-4 w-4 text-[#CD322D]" />
-                  Download audio and video from diverse sources
+                  Occaecat amet reprehenderit consequat commodo aliqua commodo.
                 </li>
                 <li>
                   <FaClipboardCheck className="mr-2 inline-block h-4 w-4 text-[#CD322D]" />
-                  Customize download format, resolution, and more
+                  Nulla culpa laborum nostrud nisi officia nostrud laborum
+                  deserunt commodo et.
                 </li>
                 <li>
                   <FaClipboardCheck className="mr-2 inline-block h-4 w-4 text-[#CD322D]" />
-                  Supports a wide range of media formats
+                  Magna dolore eiusmod ex qui magna laboris laboris eu dolore
+                  mollit labore nulla.
                 </li>
                 <li>
                   <FaClipboardCheck className="mr-2 inline-block h-4 w-4 text-[#CD322D]" />
-                  Seamless integration with Node.js and other environments
+                  Commodo minim minim elit esse qui anim aliqua incididunt aute
+                  esse excepteur excepteur.
                 </li>
               </motion.ul>
-              <button className="inline-flex h-10 items-center justify-center rounded-2xl border hover:border-neutral-900 text-[#CD322D] font-black border-[#CD322D]/50 bg-neutral-900 hover:bg-[#CD322D] hover:text-neutral-900 px-8 text-sm duration-700 transition-transform hover:scale-105 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[#CD322D] disabled:pointer-events-none disabled:opacity-50">
-                <HiFolderDownload className="mr-2 h-5 w-5" />
-                Try Yt-Dlx in Graphical User Interface mode
-              </button>
             </div>
             <motion.img
               alt="logo"
               width={550}
               height={310}
-              src="/YouTube_Circle.gif"
+              src="/eq.gif"
               {...FromRightToLeft}
               className="mx-auto aspect-video overflow-hidden rounded-2xl object-cover object-center sm:w-full border-4 border-[#cd322d6e] shadow-[0_0_400px_rgba(255,0,0,0.5)] shadow-[#CD322D]"
             />
           </div>
         </div>
       </motion.section>
-    </react.Fragment>
+      <FootPackage />
+    </main>
   );
 }
