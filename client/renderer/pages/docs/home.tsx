9b5dcf1d3355a8114bd38fea930429723c77386f
yt-dlx <yt-dlx@proton.me>
2024-06-10 21:17:12 +0530

    < npm 13.0.0 />

--- client/renderer/pages/docs/home.tsx
+++ client/renderer/pages/docs/home.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { motion } from "framer-motion";
-import NavPackage from "../components/nav";
-import FootPackage from "../components/foot";
-import Introduction from "../home/Introduction";
+import NavPackage from "../../components/nav";
+import { MdDescription } from "react-icons/md";
+import FootPackage from "../../components/foot";
 import { FaClipboardCheck } from "react-icons/fa";
+import { BiLogoTypescript } from "react-icons/bi";
+import { RiJavascriptFill } from "react-icons/ri";
+import Introduction from "../../home/Introduction";
 
 const FromLeftToRight = {
   initial: { opacity: 0, x: -100 },
@@ -25,48 +28,106 @@ const AndBounce = {
   exit: { opacity: 0, y: -50, transition: { duration: 0.3 } },
 };
 
-export default function HomePage(): JSX.Element {
+export default function AudioHighest(): JSX.Element {
   return (
     <main className="flex flex-col overflow-x-hidden max-h-screen scrollbar-thin bg-neutral-950 scrollbar-track-neutral-950 scrollbar-thumb-[#CD322D] font-semibold">
       <NavPackage />
       <Introduction />
-      <motion.section
-        id="Features"
-        className="flex items-center justify-center border-b-8 border-double border-[#cd322d6e] w-full py-12 md:py-24 lg:py-32 bg-black text-white"
-      >
+      <motion.section className="flex items-center justify-center border-b-8 border-double border-[#cd322d6e] w-full py-8 md:py-16 lg:py-24 bg-black text-white">
         <div className="container px-4 md:px-6">
-          <div className="grid items-center gap-6 lg:grid-cols-[1fr_500px] lg:gap-12 xl:grid-cols-[1fr_550px]">
+          <div className="grid items-center gap-6 lg:grid-cols-2 lg:gap-12">
             <div className="flex flex-col justify-center space-y-4">
               <motion.div className="inline-block rounded-2xl bg-[#CD322D] text-neutral-900 cursor-not-allowed font-black px-3 py-1 text-sm">
-                placeholder
+                Viewing Documentation For:
               </motion.div>
               <motion.h2
-                className="text-3xl font-black tracking-tighter sm:text-7xl text-[#CD322D]"
+                className="text-3xl font-black tracking-tighter sm:text-5xl lg:text-7xl text-[#CD322D]"
                 {...FromLeftToRight}
               >
-                Laboris exercitation velit ex duis dolor labore deserunt.
+                YtDlx.AudioOnly.Single.Highest()
               </motion.h2>
               <motion.ul className="grid gap-2 py-4" {...AndBounce}>
                 <li>
                   <FaClipboardCheck className="mr-2 inline-block h-4 w-4 text-[#CD322D]" />
-                  Occaecat amet reprehenderit consequat commodo aliqua commodo.
+                  Downloads and processes the highest quality audio from a
+                  single YouTube video.
                 </li>
                 <li>
                   <FaClipboardCheck className="mr-2 inline-block h-4 w-4 text-[#CD322D]" />
-                  Nulla culpa laborum nostrud nisi officia nostrud laborum
-                  deserunt commodo et.
+                  @param query - The YouTube video URL or ID or name.
                 </li>
                 <li>
                   <FaClipboardCheck className="mr-2 inline-block h-4 w-4 text-[#CD322D]" />
-                  Magna dolore eiusmod ex qui magna laboris laboris eu dolore
-                  mollit labore nulla.
+                  @param output - (optional) The output directory for the
+                  processed file.
                 </li>
                 <li>
                   <FaClipboardCheck className="mr-2 inline-block h-4 w-4 text-[#CD322D]" />
-                  Commodo minim minim elit esse qui anim aliqua incididunt aute
-                  esse excepteur excepteur.
+                  @param stream - (optional) Whether to stream the processed
+                  video or not.
+                </li>
+                <li>
+                  <FaClipboardCheck className="mr-2 inline-block h-4 w-4 text-[#CD322D]" />
+                  @param verbose - (optional) Whether to log verbose output or
+                  not.
+                </li>
+                <li>
+                  <FaClipboardCheck className="mr-2 inline-block h-4 w-4 text-[#CD322D]" />
+                  @param filter - (optional) The audio filter to apply.
+                  Available options: echo, slow, speed, phaser, flanger,
+                  panning, reverse, vibrato, subboost, surround, bassboost,
+                  nightcore, superslow, vaporwave, superspeed.
+                </li>
+                <li>
+                  <FaClipboardCheck className="mr-2 inline-block h-4 w-4 text-[#CD322D]" />
+                  @param onionTor - (optional) Whether to use Tor for the
+                  download or not.
+                </li>
+                <li>
+                  <FaClipboardCheck className="mr-2 inline-block h-4 w-4 text-[#CD322D]" />
+                  @returns A Promise that resolves with either `void` (if
+                  `stream` is false) or an object containing the `ffmpeg`
+                  instance and the output filename (if `stream` is true).
                 </li>
               </motion.ul>
+              <motion.div className="flex flex-wrap gap-2">
+                <button
+                  className="inline-flex h-10 items-center justify-center rounded-2xl border hover:border-neutral-900 text-[#CD322D] font-black border-[#CD322D]/50 bg-neutral-900 hover:bg-[#CD322D] hover:text-neutral-900 px-4 md:px-8 text-sm duration-700 transition-transform hover:scale-105 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[#CD322D] disabled:pointer-events-none disabled:opacity-50"
+                  onClick={() => {
+                    window.open(
+                      "https://github.com/yt-dlx/yt-dlx/blob/main/config/examples/typescript/Audio/single.highest.test.ts",
+                      "_blank"
+                    );
+                  }}
+                >
+                  <BiLogoTypescript className="mr-2 h-5 w-5" />
+                  Typescript Example
+                </button>
+                <button
+                  className="inline-flex h-10 items-center justify-center rounded-2xl border hover:border-neutral-900 text-[#CD322D] font-black border-[#CD322D]/50 bg-neutral-900 hover:bg-[#CD322D] hover:text-neutral-900 px-4 md:px-8 text-sm duration-700 transition-transform hover:scale-105 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[#CD322D] disabled:pointer-events-none disabled:opacity-50"
+                  onClick={() => {
+                    window.open(
+                      "https://github.com/yt-dlx/yt-dlx/blob/main/config/examples/javascript/Audio/single.highest.test.js",
+                      "_blank"
+                    );
+                  }}
+                >
+                  <RiJavascriptFill className="mr-2 h-5 w-5" />
+                  Javascript Example
+                </button>
+                <button
+                  className="inline-flex h-10 items-center justify-center rounded-2xl border hover:border-neutral-900 text-[#CD322D] font-black border-[#CD322D]/50 bg-neutral-900 hover:bg-[#CD322D] hover:text-neutral-900 px-4 md:px-8 text-sm duration-700 transition-transform hover:scale-105 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[#CD322D] disabled:pointer-events-none disabled:opacity-50"
+                  onClick={() => {
+                    window.open(
+                      "https://github.com/yt-dlx/yt-dlx/blob/main/config/examples/ecmascript/Audio/single.highest.test.mjs",
+                      "_blank"
+                    );
+                  }}
+                >
+                  <MdDescription className="mr-2 h-5 w-5" />
+                  EcmaScript Example
+                </button>
+              </motion.div>
             </div>
             <motion.img
               alt="logo"
@@ -74,7 +135,7 @@ export default function HomePage(): JSX.Element {
               height={310}
               src="/eq.gif"
               {...FromRightToLeft}
-              className="mx-auto aspect-video overflow-hidden rounded-2xl object-cover object-center sm:w-full border-4 border-[#cd322d6e] shadow-[0_0_400px_rgba(255,0,0,0.5)] shadow-[#CD322D]"
+              className="mx-auto aspect-video overflow-hidden rounded-2xl object-cover object-center w-full max-w-[550px] border-4 border-[#cd322d6e] shadow-[0_0_400px_rgba(255,0,0,0.5)] shadow-[#CD322D]"
             />
           </div>
         </div>
