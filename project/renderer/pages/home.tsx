9f11ef552ec4112e4c8c43ec83f4e257c290a0f2
yt-dlx <yt-dlx@proton.me>
2024-06-10 08:54:14 +0530

    < npm 12.4.0 />

---
+++ project/renderer/pages/home.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import Head from 'next/head'
+import Link from 'next/link'
+import Image from 'next/image'
+
+export default function HomePage() {
+  const [message, setMessage] = React.useState('No message found')
+
+  React.useEffect(() => {
+    window.ipc.on('message', (message: string) => {
+      setMessage(message)
+    })
+  }, [])
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Home - Nextron (basic-lang-typescript)</title>
+      </Head>
+      <div>
+        <p>
+          ⚡ Electron + Next.js ⚡ -<Link href="/next">Go to next page</Link>
+        </p>
+        <Image
+          src="/images/logo.png"
+          alt="Logo image"
+          width={256}
+          height={256}
+        />
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            window.ipc.send('message', 'Hello')
+          }}
+        >
+          Test IPC
+        </button>
+        <p>{message}</p>
+      </div>
+    </React.Fragment>
+  )
+}
