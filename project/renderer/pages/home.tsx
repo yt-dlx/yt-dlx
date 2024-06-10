3b5cd58c006a8e820ebed37741164ead357bd06d
yt-dlx <yt-dlx@proton.me>
2024-06-10 10:28:21 +0530

    < npm 13 />

--- project/renderer/pages/home.tsx
+++ project/renderer/pages/home.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import Head from "next/head";
-import Image from "next/image";
 
 export default function HomePage() {
-  const [time, setMessage] = React.useState("No time found");
+  const [time, setTime] = React.useState("No time found");
   React.useEffect(() => {
-    window.ipc.on("time", (time: string) => setMessage(time));
+    window.ipc.on("time", (time: string) => setTime(time));
   }, []);
 
   return (
@@ -15,23 +14,14 @@ export default function HomePage() {
       </Head>
       <div className="flex flex-col items-center justify-center min-h-screen py-2">
         <p className="text-xl mb-4">⚡ Electron + Next.js ⚡ - </p>
-        <Image
-          width={256}
-          height={256}
-          src="/logo.png"
-          alt="Logo image"
-          className="mb-4"
-        />
       </div>
-      <div className="flex flex-col items-center mt-4">
-        <button
-          onClick={() => window.ipc.send("time", new Date().toISOString())}
-          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-        >
-          new Date().toISOString()
-        </button>
-        <p className="mt-2 text-gray-700">{time}</p>
-      </div>
+      <button
+        onClick={() => window.ipc.send("time", new Date().toISOString())}
+        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+      >
+        Get Current Time
+      </button>
+      <p className="mt-2 text-gray-700">{time}</p>
     </React.Fragment>
   );
 }
