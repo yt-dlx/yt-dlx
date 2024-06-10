bcedb798af0daed75216b95120178c9f334145ef
yt-dlx <yt-dlx@proton.me>
2024-06-10 15:15:59 +0530

    < npm 13.0.0 />

--- client/renderer/pages/home.tsx
+++ client/renderer/pages/home.tsx
@@ -10,8 +10,8 @@ import { TbHelpHexagonFilled } from "react-icons/tb";
 
 export default function HomePage(): JSX.Element {
   return (
-    <main className="flex flex-col overflow-x-hidden max-h-screen scrollbar-thin bg-neutral-900 scrollbar-track-neutral-900 scrollbar-thumb-[#CD322D] font-semibold">
-      <motion.section className="flex items-center justify-center border-b-4 border-[#CD322D] w-full pt-12 md:pt-24 lg:pt-32 bg-neutral-900 text-white">
+    <main className="flex flex-col overflow-x-hidden max-h-screen scrollbar-thin bg-neutral-950 scrollbar-track-neutral-950 scrollbar-thumb-[#CD322D] font-semibold">
+      <motion.section className="flex items-center justify-center border-b-8 border-t-8 border-double border-[#CD322D] w-full pt-12 md:pt-24 lg:pt-32 bg-neutral-950 text-white">
         <div className="container space-y-10 xl:space-y-16">
           <div className="flex flex-col items-center space-y-4 text-center">
             <div className="space-y-2">
@@ -35,7 +35,7 @@ export default function HomePage(): JSX.Element {
               </Link>
               <Link
                 href="#"
-                className="inline-flex h-10 items-center justify-center rounded-2xl border border-neutral-900 bg-neutral-800 px-8 text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[#CD322D] disabled:pointer-events-none disabled:opacity-50"
+                className="inline-flex h-10 items-center justify-center rounded-2xl border border-neutral-950 bg-black px-8 text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[#CD322D] disabled:pointer-events-none disabled:opacity-50"
                 prefetch={false}
               >
                 <TbPackages className="mr-2 h-5 w-5" />
@@ -52,12 +52,12 @@ export default function HomePage(): JSX.Element {
           />
         </div>
       </motion.section>
-      <motion.section className="flex items-center justify-center border-b-4 border-[#CD322D] w-full py-12 md:py-24 lg:py-32 bg-neutral-800 text-white">
+      <motion.section className="flex items-center justify-center border-b-8 border-double border-[#CD322D] w-full py-12 md:py-24 lg:py-32 bg-black text-white">
         <div className="container px-4 md:px-6">
           <div className="grid items-center gap-6 lg:grid-cols-[1fr_500px] lg:gap-12 xl:grid-cols-[1fr_550px]">
             <div className="flex flex-col justify-center space-y-4">
               <div className="space-y-2">
-                <div className="inline-block rounded-2xl bg-neutral-900 px-3 py-1 text-sm">
+                <div className="inline-block rounded-2xl bg-neutral-950 px-3 py-1 text-sm">
                   Key Features
                 </div>
                 <h2 className="text-3xl font-black tracking-tighter sm:text-5xl text-[#CD322D]">
@@ -93,65 +93,25 @@ export default function HomePage(): JSX.Element {
               alt="logo"
               width={550}
               height={310}
-              src="/youtube(1).png"
-              className="mx-auto aspect-video overflow-hidden rounded-2xl object-cover object-center sm:w-full border-2 border-[#CD322D] shadow-[0_0_400px_rgba(255,0,0,0.5)] shadow-[#CD322D]"
+              src="/YouTube_Circle.gif"
+              className="mx-auto aspect-video overflow-hidden rounded-2xl object-cover object-center sm:w-full border-4 border-[#CD322D] shadow-[0_0_400px_rgba(255,0,0,0.5)] shadow-[#CD322D]"
             />
           </div>
         </div>
       </motion.section>
-      <motion.section className="flex items-center justify-center border-b-4 border-[#CD322D] w-full py-12 md:py-24 lg:py-32 bg-neutral-900 text-white">
+      <motion.section className="flex items-center justify-center border-b-8 border-double border-[#CD322D] w-full py-12 md:py-24 lg:py-32 bg-neutral-950 text-white">
         <div className="container px-4 md:px-6">
           <div className="grid items-center gap-6 lg:grid-cols-[1fr_500px] lg:gap-12 xl:grid-cols-[1fr_550px]">
             <img
               alt="logo"
               width={550}
               height={310}
-              src="/youtube-wood.png"
-              className="mx-auto aspect-video overflow-hidden rounded-2xl object-cover object-center sm:w-full border-2 border-[#CD322D] shadow-[0_0_400px_rgba(255,0,0,0.5)] shadow-[#CD322D]"
+              src="/YouTube_Music.gif"
+              className="mx-auto aspect-video overflow-hidden rounded-2xl object-cover object-center sm:w-full border-4 border-[#CD322D] shadow-[0_0_400px_rgba(255,0,0,0.5)] shadow-[#CD322D]"
             />
             <div className="flex flex-col justify-center space-y-4">
               <div className="space-y-2">
-                <div className="inline-block rounded-2xl bg-neutral-800 px-3 py-1 text-sm">
-                  Usage
-                </div>
-                <h2 className="text-3xl font-black tracking-tighter sm:text-5xl text-[#CD322D]">
-                  Seamless Integration and Usage
-                </h2>
-                <p className="max-w-[600px] text-gray-300 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                  Yt-Dlx is designed to be easy to use and integrate into your
-                  projects. Follow our step-by-step guides to install the npm
-                  package and start downloading multimedia content with just a
-                  few lines of code.
-                </p>
-              </div>
-              <div className="flex flex-col sm:flex-row items-center space-y-4 sm:space-y-0 sm:space-x-4">
-                <Link
-                  href="#"
-                  className="inline-flex h-10 items-center justify-center rounded-2xl bg-[#CD322D] px-8 text-sm font-medium text-white shadow-[0_0_400px_rgba(255,0,0,0.5)] shadow-[#CD322D] transition-colors hover:bg-[#CC0000] focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[#CD322D] disabled:pointer-events-none disabled:opacity-50"
-                  prefetch={false}
-                >
-                  <TbPackages className="mr-2 h-5 w-5" />
-                  Install on npm
-                </Link>
-                <Link
-                  href="#"
-                  className="inline-flex h-10 items-center justify-center rounded-2xl border border-neutral-900 bg-neutral-800 px-8 text-sm font-medium shadow-[0_0_400px_rgba(255,0,0,0.5)] shadow-[#CD322D] transition-colors hover:bg-neutral-900 hover:text-white focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[#CD322D] disabled:pointer-events-none disabled:opacity-50"
-                  prefetch={false}
-                >
-                  <IoBook className="mr-2 h-5 w-5" />
-                  Usage Guide
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>
-      </motion.section>
-      <motion.section className="flex items-center justify-center border-b-4 border-[#CD322D] w-full py-12 md:py-24 lg:py-32 bg-neutral-800 text-white">
-        <div className="container px-4 md:px-6">
-          <div className="grid items-center gap-6 lg:grid-cols-[1fr_500px] lg:gap-12 xl:grid-cols-[1fr_550px]">
-            <div className="flex flex-col justify-center space-y-4">
-              <div className="space-y-2">
-                <div className="inline-block rounded-2xl bg-neutral-900 px-3 py-1 text-sm">
+                <div className="inline-block rounded-2xl bg-neutral-950 px-3 py-1 text-sm">
                   Documentation
                 </div>
                 <h2 className="text-3xl font-black tracking-tighter sm:text-5xl text-[#CD322D]">
@@ -174,7 +134,7 @@ export default function HomePage(): JSX.Element {
                 </Link>
                 <Link
                   href="#"
-                  className="inline-flex h-10 items-center justify-center rounded-2xl border border-neutral-900 bg-neutral-800 px-8 text-sm font-medium shadow-[0_0_400px_rgba(255,0,0,0.5)] shadow-[#CD322D] transition-colors hover:bg-neutral-900 hover:text-white focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[#CD322D] disabled:pointer-events-none disabled:opacity-50"
+                  className="inline-flex h-10 items-center justify-center rounded-2xl border border-neutral-950 bg-black px-8 text-sm font-medium shadow-[0_0_400px_rgba(255,0,0,0.5)] shadow-[#CD322D] transition-colors hover:bg-neutral-950 hover:text-white focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[#CD322D] disabled:pointer-events-none disabled:opacity-50"
                   prefetch={false}
                 >
                   <TbHelpHexagonFilled className="mr-2 h-5 w-5" />
@@ -182,29 +142,15 @@ export default function HomePage(): JSX.Element {
                 </Link>
               </div>
             </div>
-            <img
-              alt="logo"
-              width={550}
-              height={310}
-              src="/api.png"
-              className="mx-auto aspect-video overflow-hidden rounded-2xl object-cover object-center sm:w-full border-2 border-[#CD322D] shadow-[0_0_400px_rgba(255,0,0,0.5)] shadow-[#CD322D]"
-            />
           </div>
         </div>
       </motion.section>
-      <motion.section className="flex items-center justify-center border-b-4 border-[#CD322D] w-full py-12 md:py-24 lg:py-32 bg-neutral-900 text-white">
+      <motion.section className="flex items-center justify-center border-b-8 border-double border-[#CD322D] w-full py-12 md:py-24 lg:py-32 bg-black text-white">
         <div className="container px-4 md:px-6">
           <div className="grid items-center gap-6 lg:grid-cols-[1fr_500px] lg:gap-12 xl:grid-cols-[1fr_550px]">
-            <img
-              src="/logo.png"
-              width="550"
-              height="310"
-              alt="Support"
-              className="mx-auto aspect-video overflow-hidden rounded-2xl object-cover object-center sm:w-full border-2 border-[#CD322D] shadow-[0_0_400px_rgba(255,0,0,0.5)] shadow-[#CD322D]"
-            />
             <div className="flex flex-col justify-center space-y-4">
               <div className="space-y-2">
-                <div className="inline-block rounded-2xl bg-neutral-800 px-3 py-1 text-sm">
+                <div className="inline-block rounded-2xl bg-black px-3 py-1 text-sm">
                   Support
                 </div>
                 <h2 className="text-3xl font-black tracking-tighter sm:text-5xl text-[#CD322D]">
@@ -228,7 +174,7 @@ export default function HomePage(): JSX.Element {
                 </Link>
                 <Link
                   href="#"
-                  className="inline-flex h-10 items-center justify-center rounded-2xl border border-neutral-900 bg-neutral-800 px-8 text-sm font-medium shadow-[0_0_400px_rgba(255,0,0,0.5)] shadow-[#CD322D] transition-colors hover:bg-neutral-900 hover:text-white focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[#CD322D] disabled:pointer-events-none disabled:opacity-50"
+                  className="inline-flex h-10 items-center justify-center rounded-2xl border border-neutral-950 bg-black px-8 text-sm font-medium shadow-[0_0_400px_rgba(255,0,0,0.5)] shadow-[#CD322D] transition-colors hover:bg-neutral-950 hover:text-white focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[#CD322D] disabled:pointer-events-none disabled:opacity-50"
                   prefetch={false}
                 >
                   <FaQuora className="mr-2 h-5 w-5" />
@@ -236,6 +182,13 @@ export default function HomePage(): JSX.Element {
                 </Link>
               </div>
             </div>
+            <img
+              src="/logo.png"
+              width="550"
+              height="310"
+              alt="Support"
+              className="mx-auto aspect-video overflow-hidden rounded-2xl object-cover object-center sm:w-full border-4 border-[#CD322D] shadow-[0_0_400px_rgba(255,0,0,0.5)] shadow-[#CD322D]"
+            />
           </div>
         </div>
       </motion.section>
