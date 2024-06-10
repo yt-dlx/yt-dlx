ecb59aed3e8e909cf9744adb1ab335bb6efba284
yt-dlx <yt-dlx@proton.me>
2024-06-10 12:38:16 +0530

    < npm 13.0.0 />

---
+++ project/renderer/pages/home/Playground.tsx
@@ -0,0 +1,11 @@
+import react from "react";
+
+export default function Playground(): JSX.Element {
+  const [Added, setAdd] = react.useState<any>();
+  react.useEffect(() => {
+    window.ipc.on("add", (response: string) => setAdd(response));
+  }, []);
+  // window.ipc.send("add", { num1, num2 });
+
+  return <react.Fragment></react.Fragment>;
+}
