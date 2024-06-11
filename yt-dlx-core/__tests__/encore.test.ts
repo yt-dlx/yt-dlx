a8227a251c37cbad719af9878b2c936ffff78e8b
yt-dlx <yt-dlx@proton.me>
2024-06-11 23:24:24 +0530

    < npm 12.5.0 />

--- yt-dlx-core/__tests__/encore.test.ts
+++ yt-dlx-core/__tests__/encore.test.ts
@@ -4,11 +4,7 @@ import { encore } from "../src/index";
 
 test("should find the required files with correct paths", () => {
   const dir = path.resolve(__dirname, "..");
-  expect(encore).toEqual(
-    expect.arrayContaining([
-      path.join(dir, "cprobe.exe"),
-      path.join(dir, "ffmpeg.exe"),
-      path.join(dir, "ffprobe.exe"),
-    ])
-  );
+  Object.keys(encore).forEach((file) => {
+    expect(encore[file]).toBe(path.join(dir, file));
+  });
 });
--- yt-dlx-core/src/index.ts
+++ yt-dlx-core/src/index.ts
@@ -1,16 +1,17 @@
 import * as fs from "fs";
 import * as path from "path";
 
-const getFiles = (dir: string, files: string[]): string[] => {
-  return files
-    .filter((file) => fs.existsSync(path.join(dir, file)))
-    .map((file) => path.join(dir, file));
+const getFiles = (dir: string, files: string[]): Record<string, string> => {
+  const filesObject: Record<string, string> = {};
+  files.forEach((file) => {
+    const filePath = path.join(dir, file);
+    if (fs.existsSync(filePath)) filesObject[file] = filePath;
+  });
+  return filesObject;
 };
-
 const encore = getFiles(path.resolve(__dirname, ".."), [
   "cprobe.exe",
   "ffmpeg.exe",
   "ffprobe.exe",
 ]);
-
 export { encore };
