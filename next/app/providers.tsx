cab13c11dcdff52c4b603173efd16406f32b4a2e
yt-dlx <yt-dlx@proton.me>
2024-06-12 00:43:40 +0530

    < npm 12.5.0 />

--- next/app/providers.tsx
+++ next/app/providers.tsx
@@ -3,15 +3,15 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function GlobalProviders({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  const queryClient = new QueryClient();
-  return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-      <ReactQueryDevtools />
-    </QueryClientProvider>
-  );
+    const queryClient = new QueryClient();
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+            <ReactQueryDevtools />
+        </QueryClientProvider>
+    );
 }
--- next/pages/components/nav.tsx
+++ next/pages/components/nav.tsx
@@ -2,15 +2,15 @@ import Link from "next/link";
 import { RiHome5Fill } from "react-icons/ri";
 
 export default function NavPackage() {
-  return (
-    <nav className="navbar bg-red-800/10 shadow-red-800/20 shadow-2xl text-white/60 backdrop-blur-md fixed z-50 top-0 flex justify-between items-center px-4">
-      <Link
-        href="/"
-        className="text-red-600 cursor-pointer text-4xl font-bold flex items-center gap-2 hover:scale-105 duration-500 hover:animate-pulse"
-      >
-        <RiHome5Fill size={30} />
-        yt-dlx <span className="text-sm">Copyright © 2024</span>
-      </Link>
-    </nav>
-  );
+    return (
+        <nav className="navbar bg-red-800/10 shadow-red-800/20 shadow-2xl text-white/60 backdrop-blur-md fixed z-50 top-0 flex justify-between items-center px-4">
+            <Link
+                href="/"
+                className="text-red-600 cursor-pointer text-4xl font-bold flex items-center gap-2 hover:scale-105 duration-500 hover:animate-pulse"
+            >
+                <RiHome5Fill size={30} />
+                yt-dlx <span className="text-sm">Copyright © 2024</span>
+            </Link>
+        </nav>
+    );
 }
