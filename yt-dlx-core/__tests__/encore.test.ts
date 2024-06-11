c6d670364a96f33d46d7f1963f9c5bd5fa133a71
yt-dlx <yt-dlx@proton.me>
2024-06-11 23:25:41 +0530

    < npm 12.5.0 />

--- yt-dlx-core/__tests__/encore.test.ts
+++ yt-dlx-core/__tests__/encore.test.ts
@@ -5,6 +5,10 @@ import { encore } from "../src/index";
 test("should find the required files with correct paths", () => {
   const dir = path.resolve(__dirname, "..");
   Object.keys(encore).forEach((file) => {
-    expect(encore[file]).toBe(path.join(dir, file));
+    const expectedPath = path.join(dir, file);
+    console.log(`File: ${file}`);
+    console.log(`Expected Path: ${expectedPath}`);
+    console.log(`Actual Path: ${encore[file]}`);
+    expect(encore[file]).toBe(expectedPath);
   });
 });
