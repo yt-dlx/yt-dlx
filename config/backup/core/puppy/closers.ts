8fbfa9af55b92db3698c8b49641bce9d6e4f6f60
yt-dlx <yt-dlx@proton.me>
2024-06-11 12:29:56 +0530

    < npm 12.5.0 />

--- config/backup/core/puppy/closers.ts
+++
@@ -1,7 +0,0 @@
-import { Browser, Page } from "puppeteer";
-
-export default async function closers(browser: Browser) {
-  const pages = await browser.pages();
-  await Promise.all(pages.map((page: Page) => page.close()));
-  await browser.close();
-}
