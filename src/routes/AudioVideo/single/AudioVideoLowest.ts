70593237cf7ca94fe2d4383a90bb3d040ba8c517
yt-dlx <yt-dlx@proton.me>
2024-06-13 01:12:08 +0530

    < npm 12.6.0 />

--- src/routes/AudioVideo/single/AudioVideoLowest.ts
+++ src/routes/AudioVideo/single/AudioVideoLowest.ts
@@ -1,14 +1,11 @@
-import * as fs from "fs"
-import colors from "colors"
-import * as path from "path"
-import { z, ZodError } from "zod"
-import ffmpeg from "fluent-ffmpeg"
-import { encore } from "yt-dlx-encore"
-import ytdlx from "../../../base/Agent"
-import formatTime from "../../../base/formatTime"
-import type { FfmpegCommand } from "fluent-ffmpeg"
-import calculateETA from "../../../base/calculateETA"
-import EngineOutput from "../../../interfaces/EngineOutput"
+import * as fs from "fs";
+import colors from "colors";
+import * as path from "path";
+import { z, ZodError } from "zod";
+import ffmpeg from "fluent-ffmpeg";
+import { EventEmitter } from "events";
+import { encore } from "yt-dlx-encore";
+import ytdlx from "../../../base/Agent";
 
 var ZodSchema = z.object({
   query: z.string().min(2),
@@ -28,7 +25,7 @@ var ZodSchema = z.object({
       "flipHorizontal",
     ])
     .optional(),
-})
+});
 
 /**
  * Downloads audio and video from a YouTube video URL with the lowest available resolution.
@@ -40,9 +37,10 @@ var ZodSchema = z.object({
  * @param output - (optional) The output directory for the processed file.
  * @param metadata - (optional) If true, the function returns the extracted metadata and filename without processing the audio. This can be useful for debugging or obtaining metadata without downloading the audio.
  * @param filter - (optional) The video filter to apply. Available options: "invert", "rotate90", "rotate270", "grayscale", "rotate180", "flipVertical", "flipHorizontal".
- * @returns A Promise that resolves when the audio and video processing is complete. If `stream` is true, it returns an object with the `ffmpeg` command and the `filename`.
+ *
+ * @returns An EventEmitter instance to handle events.
  */
-export default async function AudioVideoLowest({
+export default function AudioVideoLowest({
   query,
   stream,
   verbose,
@@ -50,135 +48,105 @@ export default async function AudioVideoLowest({
   metadata,
   useTor,
   filter,
-}: z.infer<typeof ZodSchema>): Promise<
-  | void
-  | { ffmpeg: FfmpegCommand; filename: string }
-  | {
-      filename: string
-      metaData: EngineOutput["metaData"]
-      ipAddress: EngineOutput["ipAddress"]
-      AudioLowF: EngineOutput["AudioLowF"]
-      AudioHighF: EngineOutput["AudioHighF"]
-      AudioLowDRC: EngineOutput["AudioLowDRC"]
-      AudioHighDRC: EngineOutput["AudioHighDRC"]
-      VideoLowF: EngineOutput["VideoLowF"]
-      VideoHighF: EngineOutput["VideoHighF"]
-      VideoLowHDR: EngineOutput["VideoLowHDR"]
-      VideoHighHDR: EngineOutput["VideoHighHDR"]
-      ManifestLow: EngineOutput["ManifestLow"]
-      ManifestHigh: EngineOutput["ManifestHigh"]
-    }
-> {
-  try {
-    ZodSchema.parse({
-      query,
-      stream,
-      verbose,
-      output,
-      metadata,
-      useTor,
-      filter,
-    })
-    var startTime: Date = new Date()
-    var engineData = await ytdlx({ query, verbose, useTor })
-    if (!engineData) {
-      throw new Error(`${colors.red("@error:")} unable to get response!`)
-    }
-    var title = engineData.metaData.title.replace(/[^a-zA-Z0-9_]+/g, "_")
-    var folder = output ? path.join(__dirname, output) : __dirname
-    if (!fs.existsSync(folder)) fs.mkdirSync(folder, { recursive: true })
-    var ff: FfmpegCommand = ffmpeg()
-      .setFfmpegPath((await encore().then(fp => fp.ffmpeg)).toString())
-      .setFfprobePath((await encore().then(fp => fp.ffprobe)).toString())
-      .addInput(engineData.AudioLowF.url)
-      .addInput(engineData.ManifestLow[0]?.url)
-      .withOutputFormat("matroska")
-      .outputOptions("-c copy")
-      .addOption("-headers", `X-Forwarded-For: ${engineData.ipAddress}`)
-    var filenameBase = `yt-dlx_(AudioVideoLowest_`
-    let filename = `${filenameBase}${filter ? filter + ")_" : ")_"}${title}.mkv`
-    var filterMap: Record<string, string[]> = {
-      grayscale: ["colorchannelmixer=.3:.4:.3:0:.3:.4:.3:0:.3:.4:.3"],
-      invert: ["negate"],
-      rotate90: ["rotate=PI/2"],
-      rotate180: ["rotate=PI"],
-      rotate270: ["rotate=3*PI/2"],
-      flipHorizontal: ["hflip"],
-      flipVertical: ["vflip"],
-    }
-    if (filter && filterMap[filter]) ff.withVideoFilter(filterMap[filter])
-    var logProgress = ({
-      percent,
-      timemark,
-    }: {
-      percent: number
-      timemark: string
-    }) => {
-      if (isNaN(percent)) percent = 0
-      percent = Math.min(Math.max(percent, 0), 100)
-      var color =
-        percent < 25 ? colors.red : percent < 50 ? colors.yellow : colors.green
-      var width = Math.floor(process.stdout.columns / 4)
-      var scomp = Math.round((width * percent) / 100)
-      var progb = color("━").repeat(scomp) + color(" ").repeat(width - scomp)
-      process.stdout.write(
-        `\r${color("@prog:")} ${progb} ${color(
-          "| @percent:",
-        )} ${percent.toFixed(2)}% ${color("| @timemark:")} ${timemark} ${color(
-          "| @eta:",
-        )} ${formatTime(calculateETA(startTime, percent))}`,
-      )
-    }
-    ff.on("error", error => {
-      throw new Error(error.message)
-    })
-      .on("start", comd => {
-        if (verbose) console.info(colors.green("@comd:"), comd)
+}: z.infer<typeof ZodSchema>): EventEmitter {
+  var emitter = new EventEmitter();
+  (async () => {
+    try {
+      ZodSchema.parse({
+        query,
+        stream,
+        verbose,
+        output,
+        metadata,
+        useTor,
+        filter,
+      });
+      var engineData = await ytdlx({ query, verbose, useTor });
+      if (!engineData) {
+        throw new Error(`${colors.red("@error:")} unable to get response!`);
+      }
+      var title = engineData.metaData.title.replace(/[^a-zA-Z0-9_]+/g, "_");
+      var folder = output ? path.join(__dirname, output) : __dirname;
+      if (!fs.existsSync(folder)) fs.mkdirSync(folder, { recursive: true });
+      var ff = ffmpeg()
+        .setFfmpegPath((await encore().then(fp => fp.ffmpeg)).toString())
+        .setFfprobePath((await encore().then(fp => fp.ffprobe)).toString())
+        .addInput(engineData.AudioLowF.url)
+        .addInput(engineData.ManifestLow[0]?.url)
+        .withOutputFormat("matroska")
+        .outputOptions("-c copy")
+        .addOption("-headers", `X-Forwarded-For: ${engineData.ipAddress}`);
+      var filenameBase = `yt-dlx_(AudioVideoLowest_`;
+      let filename = `${filenameBase}${
+        filter ? filter + ")_" : ")_"
+      }${title}.mkv`;
+      var filterMap: Record<string, string[]> = {
+        grayscale: ["colorchannelmixer=.3:.4:.3:0:.3:.4:.3:0:.3:.4:.3"],
+        invert: ["negate"],
+        rotate90: ["rotate=PI/2"],
+        rotate180: ["rotate=PI"],
+        rotate270: ["rotate=3*PI/2"],
+        flipHorizontal: ["hflip"],
+        flipVertical: ["vflip"],
+      };
+      if (filter && filterMap[filter]) ff.withVideoFilter(filterMap[filter]);
+      ff.addOption("-headers", `X-Forwarded-For: ${engineData.ipAddress}`);
+      ff.on("start", comd => {
+        if (verbose) emitter.emit("log", colors.green("@comd:"), comd);
+        emitter.emit("start", comd);
       })
-      .on("end", () => process.stdout.write("\n"))
-      .on("progress", logProgress)
-    if (stream) {
-      return {
-        ffmpeg: ff,
-        filename: output
-          ? path.join(folder, filename)
-          : filename.replace("_)_", ")_"),
+        .on("progress", progress => emitter.emit("progress", progress))
+        .on("error", error => emitter.emit("error", error.message))
+        .on("end", () => emitter.emit("end", filename));
+      switch (true) {
+        case stream:
+          emitter.emit("ready", {
+            ffmpeg: ff,
+            filename: output
+              ? path.join(folder, filename)
+              : filename.replace("_)_", ")_"),
+          });
+          break;
+        case metadata:
+          emitter.emit("metadata", {
+            filename,
+            metaData: engineData.metaData,
+            ipAddress: engineData.ipAddress,
+            AudioLowF: engineData.AudioLowF,
+            AudioHighF: engineData.AudioHighF,
+            AudioLowDRC: engineData.AudioLowDRC,
+            AudioHighDRC: engineData.AudioHighDRC,
+            VideoLowF: engineData.VideoLowF,
+            VideoHighF: engineData.VideoHighF,
+            VideoLowHDR: engineData.VideoLowHDR,
+            VideoHighHDR: engineData.VideoHighHDR,
+            ManifestLow: engineData.ManifestLow,
+            ManifestHigh: engineData.ManifestHigh,
+          });
+          break;
+        default:
+          ff.output(path.join(folder, filename.replace("_)_", ")_")))
+            .on("end", () => emitter.emit("end", filename))
+            .on("error", error => emitter.emit("error", error.message))
+            .run();
+          break;
       }
-    }
-    if (metadata) {
-      return {
-        filename,
-        metaData: engineData.metaData,
-        ipAddress: engineData.ipAddress,
-        AudioLowF: engineData.AudioLowF,
-        AudioHighF: engineData.AudioHighF,
-        AudioLowDRC: engineData.AudioLowDRC,
-        AudioHighDRC: engineData.AudioHighDRC,
-        VideoLowF: engineData.VideoLowF,
-        VideoHighF: engineData.VideoHighF,
-        VideoLowHDR: engineData.VideoLowHDR,
-        VideoHighHDR: engineData.VideoHighHDR,
-        ManifestLow: engineData.ManifestLow,
-        ManifestHigh: engineData.ManifestHigh,
+    } catch (error: any) {
+      switch (true) {
+        case error instanceof ZodError:
+          emitter.emit("error", colors.red("@zod-error:"), error.errors);
+          break;
+        default:
+          emitter.emit("error", colors.red("@error:"), error.message);
+          break;
       }
+    } finally {
+      emitter.emit(
+        "info",
+        colors.green("@info:"),
+        "❣️ Thank you for using yt-dlx. Consider 🌟starring the GitHub repo https://github.com/yt-dlx.",
+      );
     }
-    await new Promise<void>((resolve, reject) => {
-      ff.output(path.join(folder, filename.replace("_)_", ")_")))
-        .on("end", () => resolve())
-        .on("error", error =>
-          reject(new Error(colors.red("@error: ") + error.message)),
-        )
-        .run()
-    })
-  } catch (error: any) {
-    if (error instanceof ZodError) {
-      throw new Error(colors.red("@zod-error:") + error.errors)
-    }
-    throw new Error(colors.red("@error:") + error.message)
-  } finally {
-    console.log(
-      colors.green("@info:"),
-      "❣️ Thank you for using yt-dlx. Consider 🌟starring the GitHub repo https://github.com/yt-dlx.",
-    )
-  }
+  })();
+  return emitter;
 }
