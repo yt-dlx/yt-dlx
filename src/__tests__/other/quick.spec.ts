bd02a05c3ae3b4acb819353bf6d93c9bbdaaeef3
yt-dlx <yt-dlx@proton.me>
2024-06-13 00:54:39 +0530

    < npm 12.6.0 />

--- src/__tests__/other/quick.spec.ts
+++ src/__tests__/other/quick.spec.ts
@@ -1,12 +1,24 @@
-console.clear()
-import AudioHighest from "../../routes/Audio/single/AudioHighest"
-;(async () => {
-  var core = await AudioHighest({
+console.clear();
+import progbar from "../../base/progbar";
+import AudioCustom from "../../routes/Audio/single/AudioCustom";
+
+(async () => {
+  var baseTime = new Date();
+  const proc = AudioCustom({
     query: "21 savage - redrum",
+    resolution: "medium",
     metadata: false,
-    verbose: true,
+    verbose: false,
     stream: false,
     useTor: false,
-  })
-  console.log("@AudioCustom:", core)
-})()
+  });
+
+  proc.on("progress", ({ percent, timemark }) => {
+    progbar({ percent, timemark, baseTime });
+  });
+  proc.on("end", filename => console.log("@end:", filename));
+  proc.on("error", error => console.error("@error:", error));
+  proc.on("start", command => console.log("@start:", command));
+  proc.on("metadata", metadata => console.log("@metadata:", metadata));
+  proc.on("ready", ({ ffmpeg, filename }) => console.log(ffmpeg, filename));
+})();
