3355f6094e7727764dca1fbb9aa112b6b800467e
yt-dlx <yt-dlx@proton.me>
2024-06-13 12:55:38 +0530

    < npm 12.6.0 />

--- src/__tests__/other/quick.spec.ts
+++ src/__tests__/other/quick.spec.ts
@@ -1,24 +1,112 @@
 console.clear();
 import * as path from "path";
 import progbar from "../../base/progbar";
+import AudioLowest from "../../routes/Audio/single/AudioLowest";
+import AudioHighest from "../../routes/Audio/single/AudioHighest";
 import VideoLowest from "../../routes/Video/single/VideoLowest";
+import VideoHighest from "../../routes/Video/single/VideoHighest";
+import AudioVideoLowest from "../../routes/AudioVideo/single/AudioVideoLowest";
+import AudioVideoHighest from "../../routes/AudioVideo/single/AudioVideoHighest";
 
-(async () => {
-    var baseTime = new Date();
-    const proc = VideoLowest({
-        output: path.join(__dirname, "downloads"),
-        query: "21 savage - redrum",
-        metadata: false,
-        verbose: false,
-        stream: false,
-        useTor: false,
-    });
-    proc.on("progress", ({ percent, timemark }) => {
+var baseTime = new Date();
+AudioLowest({
+    output: path.join(__dirname, "downloads"),
+    query: "21 savage - redrum",
+    metadata: false,
+    verbose: false,
+    stream: false,
+    useTor: false,
+})
+    .on("progress", ({ percent, timemark }) => {
         progbar({ percent, timemark, baseTime });
-    });
-    proc.on("end", filename => console.log("@end:", filename));
-    proc.on("error", error => console.error("@error:", error));
-    proc.on("start", command => console.log("@start:", command));
-    proc.on("metadata", metadata => console.log("@metadata:", metadata));
-    proc.on("ready", ({ ffmpeg, filename }) => console.log(ffmpeg, filename));
-})();
+    })
+    .on("end", filename => console.log("@end:", filename))
+    .on("error", error => console.error("@error:", error))
+    .on("start", command => console.log("@start:", command))
+    .on("metadata", metadata => console.log("@metadata:", metadata))
+    .on("ready", ({ ffmpeg, filename }) => console.log(ffmpeg, filename));
+
+AudioHighest({
+    output: path.join(__dirname, "downloads"),
+    query: "21 savage - redrum",
+    metadata: false,
+    verbose: false,
+    stream: false,
+    useTor: false,
+})
+    .on("progress", ({ percent, timemark }) => {
+        progbar({ percent, timemark, baseTime });
+    })
+    .on("end", filename => console.log("@end:", filename))
+    .on("error", error => console.error("@error:", error))
+    .on("start", command => console.log("@start:", command))
+    .on("metadata", metadata => console.log("@metadata:", metadata))
+    .on("ready", ({ ffmpeg, filename }) => console.log(ffmpeg, filename));
+
+VideoLowest({
+    output: path.join(__dirname, "downloads"),
+    query: "21 savage - redrum",
+    metadata: false,
+    verbose: false,
+    stream: false,
+    useTor: false,
+})
+    .on("progress", ({ percent, timemark }) => {
+        progbar({ percent, timemark, baseTime });
+    })
+    .on("end", filename => console.log("@end:", filename))
+    .on("error", error => console.error("@error:", error))
+    .on("start", command => console.log("@start:", command))
+    .on("metadata", metadata => console.log("@metadata:", metadata))
+    .on("ready", ({ ffmpeg, filename }) => console.log(ffmpeg, filename));
+
+VideoHighest({
+    output: path.join(__dirname, "downloads"),
+    query: "21 savage - redrum",
+    metadata: false,
+    verbose: false,
+    stream: false,
+    useTor: false,
+})
+    .on("progress", ({ percent, timemark }) => {
+        progbar({ percent, timemark, baseTime });
+    })
+    .on("end", filename => console.log("@end:", filename))
+    .on("error", error => console.error("@error:", error))
+    .on("start", command => console.log("@start:", command))
+    .on("metadata", metadata => console.log("@metadata:", metadata))
+    .on("ready", ({ ffmpeg, filename }) => console.log(ffmpeg, filename));
+
+AudioVideoLowest({
+    output: path.join(__dirname, "downloads"),
+    query: "21 savage - redrum",
+    metadata: false,
+    verbose: false,
+    stream: false,
+    useTor: false,
+})
+    .on("progress", ({ percent, timemark }) => {
+        progbar({ percent, timemark, baseTime });
+    })
+    .on("end", filename => console.log("@end:", filename))
+    .on("error", error => console.error("@error:", error))
+    .on("start", command => console.log("@start:", command))
+    .on("metadata", metadata => console.log("@metadata:", metadata))
+    .on("ready", ({ ffmpeg, filename }) => console.log(ffmpeg, filename));
+
+AudioVideoHighest({
+    output: path.join(__dirname, "downloads"),
+    query: "21 savage - redrum",
+    metadata: false,
+    verbose: false,
+    stream: false,
+    useTor: false,
+})
+    .on("progress", ({ percent, timemark }) => {
+        progbar({ percent, timemark, baseTime });
+    })
+    .on("end", filename => console.log("@end:", filename))
+    .on("error", error => console.error("@error:", error))
+    .on("start", command => console.log("@start:", command))
+    .on("metadata", metadata => console.log("@metadata:", metadata))
+    .on("ready", ({ ffmpeg, filename }) => console.log(ffmpeg, filename));
