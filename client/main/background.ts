418c0fdc6b7843ae33296ce140cb67b3d25d39e3
yt-dlx <yt-dlx@proton.me>
2024-06-13 18:08:23 +0530

    < npm 12.7.0 />

--- client/main/background.ts
+++ client/main/background.ts
@@ -4,26 +4,30 @@ import { app } from "electron";
 import serve from "electron-serve";
 import { createWindow } from "./helpers";
 
-const isProd = process.env.NODE_ENV === "production";
-if (isProd) serve({ directory: "app" });
+const onprod = process.env.NODE_ENV === "production";
+if (onprod) serve({ directory: "app" });
 else app.setPath("userData", `${app.getPath("userData")} (development)`);
+
 (async () => {
   await app.whenReady();
   const mainWindow = createWindow("main", {
-    width: 1980,
-    height: 1080,
-    fullscreen: false,
+    fullscreen: true,
     autoHideMenuBar: true,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
   });
-  if (isProd) await mainWindow.loadURL("app://./home");
-  else {
-    const port = process.argv[2];
-    await mainWindow.loadURL(`http://localhost:${port}/home`);
-    mainWindow.webContents.openDevTools();
-    mainWindow.setFullScreen(true);
+  switch (onprod) {
+    case false:
+      await mainWindow.loadURL("http://localhost:" + process.argv[2] + "/home");
+      mainWindow.webContents.openDevTools();
+      break;
+    case true:
+    default:
+      await mainWindow.loadURL("app://./home");
+      mainWindow.webContents.openDevTools();
+      break;
   }
 })();
+
 app.on("window-all-closed", () => app.quit());
