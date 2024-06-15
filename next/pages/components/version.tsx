bfb0bc60b4ad18bfa4150896c5bbe00681da49d6
yt-dlx <yt-dlx@proton.me>
2024-06-15 22:37:55 +0530

    < npm 13.4.0 />

--- next/pages/components/version.tsx
+++ next/pages/components/version.tsx
@@ -1,7 +1,7 @@
 export default function VerPackage() {
   return (
     <div className="space-y-6">
-      <h1 className="text-5xl text-red-600 font-bold lg:text-9xl">YT-DLX@13.0.0</h1>
+      <h1 className="text-5xl text-red-600 font-bold lg:text-9xl">YT-DLX@13.4.0</h1>
     </div>
   );
 }
