d9d124b56c68eb7d8aaa9d7ddd537e4c6f430d52
yt-dlx <yt-dlx@proton.me>
2024-06-15 12:39:22 +0530

    < npm 13.0.0 />

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,28 +1,3 @@
-// import http from "http";
-// import cors from "cors";
-// import morgan from "morgan";
-// import helmet from "helmet";
-// import meta from "./routes/meta";
-// import compression from "compression";
-// import express, { Application } from "express";
-// import errorHandler from "./middlewares/errorHandler";
-// import notFoundHandler from "./middlewares/notFoundHandler";
-// const app: Application = express();
-// app.use(cors());
-// app.use(helmet());
-// app.use(morgan("dev"));
-// app.use(compression());
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use("/api", meta);
-// app.use(notFoundHandler);
-// app.use(errorHandler);
-// export default app;
-// const port = 8642 || process.env.PORT;
-// const server: http.Server = http.createServer(app);
-// server.listen(port, () => console.log("@server: listening on port", port));
-// ==============================================================================
-// server.ts
 import http from "http";
 import WebSocket from "ws";
 import Agent from "./base/Agent";
--- server/src/middlewares/notFoundHandler.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-const notFoundHandler = (req: Request, res: Response, next: NextFunction) => {
-  res.status(404).json({ message: "Not Found" });
-};
-
-export default notFoundHandler;
