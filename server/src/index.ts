e837d3b244bfc3b22056601351f7328f6fe58cfa
yt-dlx <yt-dlx@proton.me>
2024-06-15 08:41:29 +0530

    < npm 12.8.0 />

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import http from "http";
 import app from "./app";
 
-const port = 3000 || process.env.PORT;
+const port = 8642 || process.env.PORT;
 const server: http.Server = http.createServer(app);
 server.listen(port, () => console.log("@server: listening on port", port));
