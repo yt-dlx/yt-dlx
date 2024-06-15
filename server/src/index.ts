b9e9c8812776aa7e3ef08df9c30d9a398f7d6583
yt-dlx <yt-dlx@proton.me>
2024-06-15 13:09:12 +0530

    < npm 13.0.0 />

--- server/src/index.ts
+++ server/src/index.ts
@@ -31,30 +31,3 @@ const powerdown = () => {
 };
 process.on("SIGTERM", powerdown);
 process.on("SIGINT", powerdown);
-// ===========================================================================
-// import http from "http";
-// import WebSocket from "ws";
-// import Agent from "./base/Agent";
-// const server = http.createServer();
-// const wss = new WebSocket.Server({ server });
-// wss.on("connection", (ws: WebSocket) => {
-// console.log("WebSocket client connected");
-// ws.on("message", async (message: string) => {
-// try {
-// const request = JSON.parse(message);
-// if (request.action === "meta") {
-// const { query, useTor, verbose } = request.payload;
-// const proc = await Agent({ query, useTor, verbose });
-// ws.send(JSON.stringify({ success: true, data: proc }));
-// } else ws.send(JSON.stringify({ success: false, error: "Invalid action" }));
-// } catch (error: any) {
-// ws.send(JSON.stringify({ success: false, error: error.message }));
-// }
-// });
-// ws.on("close", () => console.log("WebSocket client disconnected"));
-// ws.on("error", error => console.error("WebSocket error:", error));
-// });
-// const port = process.env.PORT || 8642;
-// server.listen(port, () => console.log(`@web-socket: listening on port ${port}`));
-// server.on("error", error => console.error("Server error:", error));
-// ========================================================================
