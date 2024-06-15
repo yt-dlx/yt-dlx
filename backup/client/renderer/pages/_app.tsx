8f4c1b8b2e2ddbafe04ac63d49703b0be5883e2b
yt-dlx <yt-dlx@proton.me>
2024-06-15 08:01:09 +0530

    < npm 12.8.0 />

--- backup/client/renderer/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import "./globals.css";
-import React from "react";
-import type { AppProps } from "next/app";
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
