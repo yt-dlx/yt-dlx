24053e9f706c30a33c5d51cd33e3e5796477634f
yt-dlx <yt-dlx@proton.me>
2024-06-15 18:40:46 +0530

    < npm 13.3.0 />

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,33 +1,37 @@
-import http from "http";
-import WebSocket from "ws";
-import dotenv from "dotenv";
-import metaHandler from "./routes/metaHandler";
+// import http from "http";
+// import WebSocket from "ws";
+// import dotenv from "dotenv";
+// import metaHandler from "./routes/metaHandler";
 
-dotenv.config();
-const server = http.createServer();
-const wss = new WebSocket.Server({ server });
-wss.on("connection", (ws: WebSocket, req) => {
-  const ip = req.socket.remoteAddress;
-  console.log(`WebSocket client connected from ip: ${ip}`);
-  ws.on("message", async (message: string) => await metaHandler(ws, message));
-  ws.on("error", error => console.error(`WebSocket error: ${error.message}`));
-  ws.on("close", () => console.log(`WebSocket client disconnected from ${ip}`));
-});
+// dotenv.config();
+// const server = http.createServer();
+// const wss = new WebSocket.Server({ server });
+// wss.on("connection", (ws: WebSocket, req) => {
+// const ip = req.socket.remoteAddress;
+// console.log(`WebSocket client connected from ip: ${ip}`);
+// ws.on("message", async (message: string) => await metaHandler(ws, message));
+// ws.on("error", error => console.error(`WebSocket error: ${error.message}`));
+// ws.on("close", () => console.log(`WebSocket client disconnected from ${ip}`));
+// });
 
-const port = process.env.PORT || 8642;
-server.listen(port, () => console.log(`@web-socket: listening on port ${port}`));
-server.on("error", error => console.error("Server error:", error));
-const powerdown = () => {
-  console.log("Shutting down gracefully...");
-  wss.clients.forEach(client => client.close());
-  server.close(() => {
-    console.log("Closed out remaining connections.");
-    process.exit(0);
-  });
-  setTimeout(() => {
-    console.error("Forcing shutdown...");
-    process.exit(1);
-  }, 10000);
-};
-process.on("SIGTERM", powerdown);
-process.on("SIGINT", powerdown);
+// const port = process.env.PORT || 8642;
+// server.listen(port, () => console.log(`@web-socket: listening on port ${port}`));
+// server.on("error", error => console.error("Server error:", error));
+// const powerdown = () => {
+// console.log("Shutting down gracefully...");
+// wss.clients.forEach(client => client.close());
+// server.close(() => {
+// console.log("Closed out remaining connections.");
+// process.exit(0);
+// });
+// setTimeout(() => {
+// console.error("Forcing shutdown...");
+// process.exit(1);
+// }, 10000);
+// };
+// process.on("SIGTERM", powerdown);
+// process.on("SIGINT", powerdown);
+
+import { encore } from "yt-dlx-encore";
+
+encore().then(names => console.log(names));
