1df3dc7f62f9ce9c1853082bf17b0852ff676d01
yt-dlx <yt-dlx@proton.me>
2024-06-15 13:05:52 +0530

    < npm 13.0.0 />

--- server/src/routes/metaHandler.ts
+++ server/src/routes/metaHandler.ts
@@ -1,20 +1,31 @@
+import path from "path";
 import WebSocket from "ws";
-import Agent from "../base/Agent";
+import ytdlx from "yt-dlx";
 
 const metaHandler = (ws: WebSocket, message: string) => {
   try {
-    const request = JSON.parse(message);
-    if (request.action === "meta") {
-      const { query, useTor, verbose } = request.payload;
-      Agent({ query, useTor, verbose })
-        .then(proc => ws.send(JSON.stringify({ success: true, data: proc })))
-        .catch(error => {
-          ws.send(JSON.stringify({ success: false, error: error.message }));
+    const req = JSON.parse(message);
+    if (req.action === "meta") {
+      ytdlx.AudioOnly.Single.Highest({
+        stream: false,
+        metadata: true,
+        query: req.payload.query,
+        useTor: req.payload.useTor,
+        verbose: req.payload.verbose,
+        output: path.join(__dirname, req.payload.output),
+      })
+        .on("error", error => ws.send(error))
+        .on("end", filename => ws.send(filename))
+        .on("start", command => ws.send(command))
+        .on("progress", progress => ws.send(progress))
+        .on("metadata", metadata => ws.send(metadata))
+        .on("ready", ({ ffmpeg, filename }) => {
+          console.log(ffmpeg, filename);
         });
     } else ws.send(JSON.stringify({ success: false, error: "Invalid action" }));
   } catch (error: any) {
     if (error instanceof Error) ws.send(JSON.stringify({ success: false, error: error.message }));
-    else ws.send(JSON.stringify({ success: false, error: "Rate limit exceeded" }));
+    else ws.send(JSON.stringify({ success: false, error: "unknwon error occured" }));
   }
 };
 export default metaHandler;
