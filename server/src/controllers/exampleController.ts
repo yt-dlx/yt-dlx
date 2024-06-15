65bc4fec0a7c5e117ed7cf9763d824eb508a296d
yt-dlx <yt-dlx@proton.me>
2024-06-15 12:20:53 +0530

    < npm 13.0.0 />

--- server/src/controllers/exampleController.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response } from "express";
-
-export const getExample = (req: Request, res: Response) => {
-  res.json({ message: "This is an example response from controller" });
-};
