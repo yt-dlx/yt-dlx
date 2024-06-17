9a55953bb37809e487bd06aa5bd2d2239524d53c
magneum-II <yt-dlx@proton.me>
2024-06-17 11:46:42 +0530

    ⚡npm 13.4.0⚡

--- server/build.mjs
+++ server/build.mjs
@@ -1,10 +1,13 @@
-import { exec } from 'child_process';
-import { platform } from 'os';
+import { exec } from "child_process";
+import { platform } from "os";
 
 const commands = {
-  win32: 'tsc -p tsconfig.json && caxa --input . --output "out/server.exe" -- "{{caxa}}/node_modules/.bin/node" "{{caxa}}/build/index.js"',
-  darwin: 'tsc -p tsconfig.json && caxa --input . --output "out/server.app" -- "{{caxa}}/node_modules/.bin/node" "{{caxa}}/build/index.js"',
-  linux: 'tsc -p tsconfig.json && caxa --input . --output "out/server.sh" -- "{{caxa}}/node_modules/.bin/node" "{{caxa}}/build/index.js"'
+  win32:
+    'tsc -p tsconfig.json && caxa --input . --output "out/server.exe" -- "{{caxa}}/node_modules/.bin/node" "{{caxa}}/build/index.js"',
+  darwin:
+    'tsc -p tsconfig.json && caxa --input . --output "out/server.app" -- "{{caxa}}/node_modules/.bin/node" "{{caxa}}/build/index.js"',
+  linux:
+    'tsc -p tsconfig.json && caxa --input . --output "out/server.sh" -- "{{caxa}}/node_modules/.bin/node" "{{caxa}}/build/index.js"',
 };
 
 const command = commands[platform()];
@@ -21,4 +24,4 @@ if (command) {
     }
     console.log(`Stdout: ${stdout}`);
   });
-} else console.error('Unsupported platform');
+} else console.error("Unsupported platform");
