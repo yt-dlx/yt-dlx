798c76d61cc527639f93b7c8127d98f22775682a
magneum-II <yt-dlx@proton.me>
2024-06-19 23:19:03 +0530

    ⚡npm 13.4.0⚡

--- server/src/base/calculateETA.ts
+++
@@ -1,6 +0,0 @@
-export default function calculateETA(startTime: Date, percent: number): string {
-  var currentTime = new Date();
-  var elapsedTime = (currentTime.getTime() - startTime.getTime()) / 1000;
-  var remainingTime = (elapsedTime / percent) * (100 - percent);
-  return remainingTime.toFixed(2);
-}
