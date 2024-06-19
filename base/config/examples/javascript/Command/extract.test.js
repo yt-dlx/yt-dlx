f6182acf65874b8f4f9ad96c7184ec2bca7c0bc4
magneum-II <yt-dlx@proton.me>
2024-06-20 01:01:44 +0530

    ⚡npm 13.4.0⚡

--- base/config/examples/javascript/Command/extract.test.js
+++ base/config/examples/javascript/Command/extract.test.js
@@ -4,12 +4,12 @@ const YouTube = require("yt-dlx");
 const colors = require("colors");
 
 (async () => {
-    try {
-        await YouTube.default.info.extract({
-            verbose: true,
-            query: "video-NAME/ID/URL",
-        });
-    } catch (error) {
-        console.error(colors.red(error.message));
-    }
+ try {
+  await YouTube.default.info.extract({
+   verbose: true,
+   query: "video-NAME/ID/URL",
+  });
+ } catch (error) {
+  console.error(colors.red(error.message));
+ }
 })();
--- base/config/examples/javascript/Command/video_data.test.js
+++ base/config/examples/javascript/Command/video_data.test.js
@@ -4,12 +4,12 @@ const YouTube = require("yt-dlx");
 const colors = require("colors");
 
 (async () => {
-    try {
-        const result = await YouTube.default.ytSearch.Video.Single({
-            query: "video-NAME/ID/URL",
-        });
-        console.log(result);
-    } catch (error) {
-        console.error(colors.red(error.message));
-    }
+ try {
+  const result = await YouTube.default.ytSearch.Video.Single({
+   query: "video-NAME/ID/URL",
+  });
+  console.log(result);
+ } catch (error) {
+  console.error(colors.red(error.message));
+ }
 })();
