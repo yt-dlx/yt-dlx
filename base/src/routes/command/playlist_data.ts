413d5c31a86ffc9e01e1c95cd599b4175a8c1dc8
magneum-II <yt-dlx@proton.me>
2024-06-20 01:01:48 +0530

    ⚡npm 13.4.0⚡

--- base/src/routes/command/playlist_data.ts
+++ base/src/routes/command/playlist_data.ts
@@ -10,13 +10,13 @@ import web, { playlistVideosType } from "../../web";
  * @throws An error if the playlist link is incorrect or if unable to get a response.
  */
 export default async function playlist_data({ query }: { query: string }): Promise<playlistVideosType> {
-    var playlistId = await YouTubeID(query);
-    if (!playlistId) {
-        throw new Error(colors.red("@error: ") + "incorrect playlist link");
-    } else {
-        var metaData = await web.playlistVideos({ playlistId });
-        if (!metaData) {
-            throw new Error(colors.red("@error: ") + "Unable to get response!");
-        } else return metaData;
-    }
+ var playlistId = await YouTubeID(query);
+ if (!playlistId) {
+  throw new Error(colors.red("@error: ") + "incorrect playlist link");
+ } else {
+  var metaData = await web.playlistVideos({ playlistId });
+  if (!metaData) {
+   throw new Error(colors.red("@error: ") + "Unable to get response!");
+  } else return metaData;
+ }
 }
--- client/renderer/pages/_app.tsx
+++ client/renderer/pages/_app.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+ return <Component {...pageProps} />;
 }
 
 export default MyApp;
