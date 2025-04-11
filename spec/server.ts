fb0272d0d6e92f9e5aba676fcc6e55f8e3847340
magneum-II <yt-dlx@proton.me>
2025-04-11 06:33:36 -0700

    yt-dlx rework 2025

--- spec/server.ts
+++ spec/server.ts
@@ -4,6 +4,23 @@ import express from "express";
 const client = express();
 const PORT = process.env.PORT || 3000;
 // ====================================================================================
+client.get("/AudioOnlyCustom", async (req: any, res: any) => {
+  const query = req.query.query as string;
+  const resolution = ["high", "medium", "low", "ultralow"].includes(req.query.resolution) ? (req.query.resolution as "high" | "medium" | "low" | "ultralow") : "medium";
+  if (!query) return res.status(400).json({ error: "Missing query parameter." });
+  if (!resolution) return res.status(400).json({ error: "Missing resolution parameter." });
+  try {
+    const instance = ytdlx.AudioOnly.Custom({ useTor: true, metadata: true, verbose: true, query, resolution });
+    instance
+      .on("metadata", (metadata: any) => res.json(metadata))
+      .on("start", (command: any) => console.log(colors.gray("start:"), command))
+      .on("error", (error: any) => res.status(500).json({ error: error.message || error }));
+  } catch (err: any) {
+    console.error(colors.red("❌ Unexpected error:"), err);
+    res.status(500).json({ error: err.message });
+  }
+});
+// ====================================================================================
 client.get("/AudioOnlyHighest", async (req: any, res: any) => {
   const query = req.query.query as string;
   if (!query) return res.status(400).json({ error: "Missing query parameter." });
@@ -24,6 +41,25 @@ client.get("/AudioOnlyLowest", async (req: any, res: any) => {
   if (!query) return res.status(400).json({ error: "Missing query parameter." });
   try {
     const instance = ytdlx.AudioOnly.Lowest({ useTor: true, metadata: true, verbose: true, query });
+    instance
+      .on("metadata", (metadata: any) => res.json(metadata))
+      .on("start", (command: any) => console.log(colors.gray("start:"), command))
+      .on("error", (error: any) => res.status(500).json({ error: error.message || error }));
+  } catch (err: any) {
+    console.error(colors.red("❌ Unexpected error:"), err);
+    res.status(500).json({ error: err.message });
+  }
+});
+// ====================================================================================
+client.get("/VideoOnlyCustom", async (req: any, res: any) => {
+  const query = req.query.query as string;
+  const resolution = ["144p", "240p", "360p", "480p", "720p", "1080p", "1440p", "2160p", "3072p", "4320p", "6480p", "8640p", "12000p"].includes(req.query.resolution)
+    ? (req.query.resolution as "144p" | "240p" | "360p" | "480p" | "720p" | "1080p" | "1440p" | "2160p" | "3072p" | "4320p" | "6480p" | "8640p" | "12000p")
+    : "720p";
+  if (!query) return res.status(400).json({ error: "Missing query parameter." });
+  if (!resolution) return res.status(400).json({ error: "Missing resolution parameter." });
+  try {
+    const instance = ytdlx.VideoOnly.Custom({ useTor: true, metadata: true, verbose: true, query, resolution });
     instance
       .on("metadata", (metadata: any) => res.json(metadata))
       .on("start", (command: any) => console.log(colors.gray("start:"), command))
@@ -79,6 +115,25 @@ client.get("/AudioVideoHighest", async (req: any, res: any) => {
   }
 });
 // ====================================================================================
+client.get("/AudioVideoCustom", async (req: any, res: any) => {
+  const query = req.query.query as string;
+  const resolution = ["144p", "240p", "360p", "480p", "720p", "1080p", "1440p", "2160p", "3072p", "4320p", "6480p", "8640p", "12000p"].includes(req.query.resolution)
+    ? (req.query.resolution as "144p" | "240p" | "360p" | "480p" | "720p" | "1080p" | "1440p" | "2160p" | "3072p" | "4320p" | "6480p" | "8640p" | "12000p")
+    : "720p";
+  if (!query) return res.status(400).json({ error: "Missing query parameter." });
+  if (!resolution) return res.status(400).json({ error: "Missing resolution parameter." });
+  try {
+    const instance = ytdlx.AudioVideo.Custom({ useTor: true, metadata: true, verbose: true, query, resolution });
+    instance
+      .on("metadata", (metadata: any) => res.json(metadata))
+      .on("start", (command: any) => console.log(colors.gray("start:"), command))
+      .on("error", (error: any) => res.status(500).json({ error: error.message || error }));
+  } catch (err: any) {
+    console.error(colors.red("❌ Unexpected error:"), err);
+    res.status(500).json({ error: err.message });
+  }
+});
+// ====================================================================================
 client.get("/AudioVideoLowest", async (req: any, res: any) => {
   const query = req.query.query as string;
   if (!query) return res.status(400).json({ error: "Missing query parameter." });
@@ -94,4 +149,4 @@ client.get("/AudioVideoLowest", async (req: any, res: any) => {
   }
 });
 // ====================================================================================
-client.listen(PORT, () => console.log(colors.cyan(`🚀 Metadata API is live at http://localhost:${PORT}`)));
+client.listen(PORT, () => console.log(colors.cyan(`🚀 YT-DLX Server is live at port ${PORT}`)));
