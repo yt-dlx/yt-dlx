62f8c4d641c8f3785af5fab0145a60bc2aac0696
magneum-II <yt-dlx@proton.me>
2025-04-12 19:14:31 -0700

    yt-dlx rework 2025

--- _backup_/server/src/base/formatTime.ts
+++
@@ -1,7 +0,0 @@
-export default function formatTime(seconds: any): string {
-  if (!isFinite(seconds) || isNaN(seconds)) return "00h 00m 00s";
-  var hours = Math.floor(seconds / 3600);
-  var minutes = Math.floor((seconds % 3600) / 60);
-  var secs = Math.floor(seconds % 60);
-  return `${hours.toString().padStart(2, "0")}h ${minutes.toString().padStart(2, "0")}m ${secs.toString().padStart(2, "0")}s`;
-}
