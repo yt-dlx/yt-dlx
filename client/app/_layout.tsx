5f35a9a3122462bf4a4ce19076d0e97224096d1b
magneum-II <yt-dlx@proton.me>
2025-04-14 23:23:25 -0700

    ♥ YouTubeDLX ♥

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -1,5 +1,4 @@
 import { Stack } from "expo-router";
-
 export default function RootLayout() {
   return <Stack />;
 }
--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,14 +1,7 @@
 import { Text, View } from "react-native";
-
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
