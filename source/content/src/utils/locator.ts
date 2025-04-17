f87fc58e85ea201a8ca577cec8e0cd7b22e58660
magneum-II <yt-dlx@proton.me>
2025-04-17 07:11:56 -0700

    yt-dlx (stable)

--- source/content/src/utils/locator.ts
+++ source/content/src/utils/locator.ts
@@ -1,15 +1,17 @@
 import colors from "colors";
 import * as path from "path";
 import * as fsx from "fs-extra";
+import { spawn } from "child_process";
+
 async function getBinaryPath(execName: string): Promise<string | null> {
   try {
     const nodeModulesPath = path.join(process.cwd(), "node_modules", "yt-dlx", "pkg");
-    const binaryPath = path.join(nodeModulesPath, execName + (process.platform === "win32" ? ".exe" : ""));
+    const binaryPath = path.join(nodeModulesPath, execName + (process.platform === "win32" ? ".exe" : process.platform === "linux" ? ".bin" : ""));
     try {
       await fsx.access(binaryPath, fsx.constants.X_OK);
       return `"${binaryPath}"`;
     } catch {
-      const devPath = path.join(process.cwd(), "pkg", execName + (process.platform === "win32" ? ".exe" : ""));
+      const devPath = path.join(process.cwd(), "pkg", execName + (process.platform === "win32" ? ".exe" : process.platform === "linux" ? ".bin" : ""));
       await fsx.access(devPath, fsx.constants.X_OK);
       return `"${devPath}"`;
     }
@@ -25,8 +27,12 @@ export async function locator() {
       const execPath = await getBinaryPath(execName);
       if (execPath) results[execName] = execPath;
       else {
-        console.log(colors.yellow("@warning:"), `${execName} not found in package binary directory.`);
-        console.error(colors.red("@error:"), "please run 'yarn/npm/bun/pnpm install/add yt-dlx'");
+        console.log(colors.red("@red:"), `${execName} not found in package binary directory. Auto-Downloading....`);
+        const postdepsPath = path.join(process.cwd(), "postdeps.mjs");
+        const nodeProcess = spawn("node", [postdepsPath]);
+        nodeProcess.stdout.on("data", data => console.log(data.toString()));
+        nodeProcess.stderr.on("data", data => console.error(data.toString()));
+        nodeProcess.on("close", code => console.log(`child process exited with code ${code}`));
         results[execName] = "";
       }
     }
