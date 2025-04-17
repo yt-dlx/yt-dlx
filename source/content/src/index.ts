4ab4924258ce0b5d327545b705e65999b7c174cc
magneum-II <yt-dlx@proton.me>
2025-04-17 09:19:54 -0700

    yt-dlx (stable)

--- source/content/src/index.ts
+++ source/content/src/index.ts
@@ -1,40 +1,75 @@
 import progbar from "./utils/progbar";
-import help from "./routes/Info/Help";
+import help from "./routes/Misc/Help";
 import YouTubeID from "./utils/YouTubeId";
-import extract from "./routes/Info/Extract";
+import extract from "./routes/Misc/Extract";
 import AudioLowest from "./routes/Audio/Lowest";
 import AudioCustom from "./routes/Audio/Custom";
 import VideoLowest from "./routes/Video/Lowest";
 import VideoCustom from "./routes/Video/Custom";
+import list_formats from "./routes/Misc/Formats";
 import AudioHighest from "./routes/Audio/Highest";
 import VideoHighest from "./routes/Video/Highest";
 import home_feed from "./routes/Account/HomeFeed";
+import video_comments from "./routes/Misc/Comments";
 import video_data from "./routes/Search/video_data";
-import list_formats from "./routes/Info/Formats";
+import watch_history from "./routes/Account/History";
+import video_transcript from "./routes/Misc/Transcript";
 import channel_data from "./routes/Search/channel_data";
 import search_videos from "./routes/Search/search_videos";
 import playlist_data from "./routes/Search/playlist_data";
-import video_comments from "./routes/Info/Comments";
-import AudioVideoCustom from "./routes/AudioVideo/Custom";
-import AudioVideoLowest from "./routes/AudioVideo/Lowest";
-import watch_history from "./routes/Account/History";
-import AudioVideoHighest from "./routes/AudioVideo/Highest";
+import AudioVideoCustom from "./routes/Audio_Video/Custom";
+import AudioVideoLowest from "./routes/Audio_Video/Lowest";
+import AudioVideoHighest from "./routes/Audio_Video/Highest";
 import related_videos from "./routes/Search/related_videos";
 import search_channels from "./routes/Search/search_channels";
-import video_transcript from "./routes/Info/Transcript";
 import search_playlists from "./routes/Search/search_playlists";
 import subscriptions_feed from "./routes/Account/SubscriptionsFeed";
 import unseen_notifications from "./routes/Account/UnseenNotifications";
 var YouTubeDLX = {
-  Audio: { Lowest: AudioLowest, Highest: AudioHighest, Custom: AudioCustom },
-  Video: { Lowest: VideoLowest, Highest: VideoHighest, Custom: VideoCustom },
-  Audio_Video: { Lowest: AudioVideoLowest, Highest: AudioVideoHighest, Custom: AudioVideoCustom },
-  Search: {
-    Channel: { Single: channel_data, Multiple: search_channels },
-    Playlist: { Single: playlist_data, Multiple: search_playlists },
-    Video: { Single: video_data, Multiple: search_videos, Related: related_videos },
+  Audio: {
+    Custom: AudioCustom, // YouTubeDLX.Audio.Custom
+    Lowest: AudioLowest, // YouTubeDLX.Audio.Lowest
+    Highest: AudioHighest, // YouTubeDLX.Audio.Highest
   },
-  Account: { History: watch_history, HomeFeed: home_feed, SubscriptionsFeed: subscriptions_feed, UnseenNotifications: unseen_notifications },
-  Info: { Help: help, ProgressBar: progbar, Extract: extract, Formats: list_formats, GetVideoId: YouTubeID, Comments: video_comments, Transcript: video_transcript },
+  Video: {
+    Custom: VideoCustom, // YouTubeDLX.Video.Custom
+    Lowest: VideoLowest, // YouTubeDLX.Video.Lowest
+    Highest: VideoHighest, // YouTubeDLX.Video.Highest
+  },
+  Audio_Video: {
+    Custom: AudioVideoCustom, // YouTubeDLX.Audio_Video.Custom
+    Lowest: AudioVideoLowest, // YouTubeDLX.Audio_Video.Lowest
+    Highest: AudioVideoHighest, // YouTubeDLX.Audio_Video.Highest
+  },
+  Search: {
+    Channel: {
+      Single: channel_data, // YouTubeDLX.Search.Channel.Single
+      Multiple: search_channels, // YouTubeDLX.Search.Channel.Multiple
+    },
+    Playlist: {
+      Single: playlist_data, // YouTubeDLX.Search.Playlist.Single
+      Multiple: search_playlists, // YouTubeDLX.Search.Playlist.Multiple
+    },
+    Video: {
+      Single: video_data, // YouTubeDLX.Search.Video.Single
+      Related: related_videos, // YouTubeDLX.Search.Video.Related
+      Multiple: search_videos, // YouTubeDLX.Search.Video.Multiple
+    },
+  },
+  Account: {
+    HomeFeed: home_feed, // YouTubeDLX.Account.HomeFeed
+    History: watch_history, // YouTubeDLX.Account.History
+    SubscriptionsFeed: subscriptions_feed, // YouTubeDLX.Account.SubscriptionsFeed
+    UnseenNotifications: unseen_notifications, // YouTubeDLX.Account.UnseenNotifications
+  },
+  Misc: {
+    Help: help, // YouTubeDLX.Misc.Help
+    Extract: extract, // YouTubeDLX.Misc.Extract
+    Formats: list_formats, // YouTubeDLX.Misc.Formats
+    ProgressBar: progbar, // YouTubeDLX.Misc.ProgressBar
+    GetVideoId: YouTubeID, // YouTubeDLX.Misc.GetVideoId
+    Comments: video_comments, // YouTubeDLX.Misc.Comments
+    Transcript: video_transcript, // YouTubeDLX.Misc.Transcript
+  },
 };
 export default YouTubeDLX;
