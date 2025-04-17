572c9928fc3f62cb3636b99c09784d86dfdff86b
magneum-II <yt-dlx@proton.me>
2025-04-17 05:58:15 -0700

    yt-dlx (stable)

---
+++ source/content/postdeps.mjs
@@ -0,0 +1,55 @@
+import axios from "axios";
+import { join } from "path";
+import { createWriteStream, existsSync, mkdirSync } from "fs";
+const outputDir = join(process.cwd(), "pkg");
+if (!existsSync(outputDir)) mkdirSync(outputDir, { recursive: true });
+const binDL = async (url, filepath, binaryName) => {
+  try {
+    const response = await axios({ url, method: "GET", responseType: "stream" });
+    if (response.status !== 200) throw new Error("@error: " + response.statusText);
+    const tSize = parseInt(response.headers["content-length"], 10);
+    const writer = createWriteStream(filepath);
+    let dSize = 0;
+    response.data.on("data", chunk => {
+      dSize += chunk.length;
+      const progress = Math.round((dSize / tSize) * 100);
+      process.stdout.write("[" + binaryName + "]: " + progress + "%\\r");
+    });
+    response.data.pipe(writer);
+    await new Promise((resolve, reject) => {
+      writer.on("finish", resolve);
+      writer.on("error", reject);
+    });
+  } catch (error) {
+    console.error("@error: " + binaryName + ": " + error.message);
+  }
+};
+const main = async () => {
+  const binaries = [
+    { name: "ffmpeg.exe", url: "https://github.com/yt-dlx/yt-dlx/releases/latest/download/ffmpeg.exe" },
+    { name: "cprobe.exe", url: "https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe" },
+    { name: "ffprobe.exe", url: "https://github.com/yt-dlx/yt-dlx/releases/latest/download/ffprobe.exe" },
+  ];
+  for (const binary of binaries) {
+    const filepath = join(outputDir, binary.name);
+    if (!existsSync(filepath)) await binDL(binary.url, filepath, binary.name);
+  }
+};
+main();
+
+const cprobe = {
+  Windows: { name: "cprobe.exe", url: "https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe" },
+  Linux: [
+    { name: "cprobe_linux", url: "https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_linux" },
+    { name: "cprobe_linux_armv7l", url: "https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_linux_armv7l" },
+    { name: "cprobe_linux_aarch64", url: "https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_linux_aarch64" },
+  ],
+};
+const ffmpeg = {
+  Windows: { name: "ffmpeg.exe", url: "https://github.com/BtbN/FFmpeg-Builds/releases/latest/download/ffmpeg-master-latest-win64-gpl.zip" },
+  Linux: [
+    { name: "ffmpeg_linux_x86_64", url: "https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-arm64-static.tar.xz" },
+    { name: "ffmpeg_linux_armhf", url: "https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-armhf-static.tar.xz" },
+    { name: "ffmpeg_linux_arm64", url: "https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-arm64-static.tar.xz" },
+  ],
+};
