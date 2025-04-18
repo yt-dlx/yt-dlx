dd6445ed6de789a7bcc775b833cad930f9af23e7
magneum-II <yt-dlx@proton.me>
2025-04-18 03:25:24 -0700

    yt-dlx (Tor)

--- source/content/src/spec/quick.spec.ts
+++ source/content/src/spec/quick.spec.ts
@@ -3,9 +3,6 @@ import colors from "colors";
 import YouTubeDLX from "..";
 dotenv.config();
 console.clear();
-// YouTubeDLX.Misc.Video.Extract({ useTor: true, verbose: true, query: "Tera Hokey Rahu" })
-// .on("error", error => console.error("Error:", error))
-// .on("data", data => console.log("Data:", data));
-YouTubeDLX.Audio.Highest({ query: "Dil Darbadar" })
+YouTubeDLX.Video.Highest({ query: "Dil Darbadar" })
   .on("data", data => console.log(colors.italic.green("@data:"), data))
   .on("error", error => console.error(colors.italic.red("@error:"), error));
