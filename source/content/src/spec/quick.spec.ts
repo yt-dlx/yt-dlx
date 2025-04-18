81da26c23991f0001ce1b6015ef88c9c2fb64257
magneum-II <yt-dlx@proton.me>
2025-04-17 22:10:17 -0700

    yt-dlx (Tor)

--- source/content/src/spec/quick.spec.ts
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-import colors from "colors";
-import YouTubeDLX from "..";
-dotenv.config();
-console.clear();
-YouTubeDLX.Misc.Video.Extract({ useTor: true, verbose: false, query: "Tera Hokey Rahu" })
-  .on("error", error => console.error("Error:", error))
-  .on("data", data => console.log("Data:", data));
--- source/website/src/app/docs/Audio/Custom.tsx
+++
@@ -1,5 +0,0 @@
-"use client";
-import React from "react";
-import { motion } from "framer-motion";
-import NavPackage from "@/components/nav";
-import FootPackage from "@/components/foot";
