5bf490939859b807bf76a6f7915f12b41d230314
magneum-II <yt-dlx@proton.me>
2025-04-18 19:41:47 -0700

    ♥ yt-dlx ♥

--- source/website/src/app/home/page.tsx
+++ source/website/src/app/home/page.tsx
@@ -1,10 +1,10 @@
 import react from "react";
 import { motion } from "framer-motion";
 import { FaYarn } from "react-icons/fa";
-import NpmModel from "./models/NpmModel";
-import BunModel from "./models/BunModel";
-import YarnModel from "./models/YarnModel";
-import PnpmModel from "./models/PnpmModel";
+import NpmModel from "./models/npm";
+import BunModel from "./models/bun";
+import YarnModel from "./models/yarn";
+import PnpmModel from "./models/pnpm";
 import { TbBrandNpm } from "react-icons/tb";
 import { SiBun, SiPnpm } from "react-icons/si";
 const FromTopToBottom = { initial: { opacity: 0, y: -100 }, exit: { opacity: 0, y: -50, transition: { duration: 0.3 } }, whileInView: { opacity: 1, y: 0, transition: { duration: 0.8 } } };
