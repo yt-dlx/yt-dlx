c5a7e2fddbc8a0db9a8e38520afe01000903a426
magneum-II <yt-dlx@proton.me>
2025-04-18 21:48:55 -0700

    yt-dlx (multi)

--- push.py
+++ push.py
@@ -33,7 +33,6 @@ def monitor_and_commit():
                 print(push_command.stderr)
                 print("Commit and push successful.")
             else:
-                print("No changes detected. Waiting 30s...")
                 time.sleep(30)
         except subprocess.CalledProcessError as e:
             print(f"Git command failed: {e}")
